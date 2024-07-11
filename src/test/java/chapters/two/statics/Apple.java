962725b2f9507c660c6b4449d09dae1fe5b977a3
andrey shalagin <andreyshalagin@gmail.com>
2024-07-11 08:37:13 +0300

    Пример автоупаковки: расчёт веса яблок

--- src/test/java/chapters/two/statics/Apple.java
+++ src/test/java/chapters/two/statics/Apple.java
@@ -1,5 +1,7 @@
 package chapters.two.statics;
 
+import java.util.Arrays;
+
 public class Apple {
     static int fruitId = 545;
     String color;
@@ -8,5 +10,9 @@ public class Apple {
     public Apple(String color, int weight) {
         this.color = color;
         this.weight = weight;
+    }
+
+    public static Integer getAllFruitsWeight(Apple... apples){
+        return Arrays.stream(apples).map(a -> a.weight).mapToInt(Integer::intValue).sum();
     }
 }
--- src/test/java/chapters/two/statics/AppleTestDrive.java
+++ src/test/java/chapters/two/statics/AppleTestDrive.java
@@ -1,5 +1,7 @@
 package chapters.two.statics;
 
+import java.util.ArrayList;
+
 public class AppleTestDrive {
     public static void main(String[] args) {
         Apple redApple = new Apple("red", 150);
@@ -13,5 +15,12 @@ public class AppleTestDrive {
         System.out.println("red apple имеет вес " + redApple.weight);
         System.out.println("green apple имеет цвет " + greenApple.color);
         System.out.println("green apple имеет вес " + greenApple.weight);
+
+        Apple[] apples = new Apple[]{
+                redApple,
+                greenApple
+        };
+        Integer allFruitsWeight = Apple.getAllFruitsWeight(apples);
+        System.out.println("Общий вес всех яблок = " + allFruitsWeight);
     }
 }
