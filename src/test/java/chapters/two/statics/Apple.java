d62938d71a557f06dd1d0d12834dcd773f53a428
andrey shalagin <andreyshalagin@gmail.com>
2024-07-11 08:31:22 +0300

    Особенности статических и не статических данных

---
+++ src/test/java/chapters/two/statics/Apple.java
@@ -0,0 +1,12 @@
+package chapters.two.statics;
+
+public class Apple {
+    static int fruitId = 545;
+    String color;
+    int weight;
+
+    public Apple(String color, int weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+}
---
+++ src/test/java/chapters/two/statics/AppleTestDrive.java
@@ -0,0 +1,17 @@
+package chapters.two.statics;
+
+public class AppleTestDrive {
+    public static void main(String[] args) {
+        Apple redApple = new Apple("red", 150);
+        Apple greenApple = new Apple("green", 100);
+        System.out.println("Все яблоки имеют один идентификатор для своего класса фруктов");
+        System.out.println("redApple.fruitId " + redApple.fruitId);
+        System.out.println("green.fruitId " + greenApple.fruitId);
+
+        System.out.println("Но каждое яблоко уникально");
+        System.out.println("red apple имеет цвет " + redApple.color);
+        System.out.println("red apple имеет вес " + redApple.weight);
+        System.out.println("green apple имеет цвет " + greenApple.color);
+        System.out.println("green apple имеет вес " + greenApple.weight);
+    }
+}
