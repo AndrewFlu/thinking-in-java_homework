dbeccc8e9416108d1a588741d0ea1251e402c209
andrey shalagin <andreyshalagin@gmail.com>
2024-07-12 07:42:09 +0300

    javadoc комментарии

--- src/test/java/chapters/two/HelloWorld.java
+++ src/test/java/chapters/two/HelloWorld.java
@@ -1,6 +1,15 @@
 package chapters.two;
 
+/**
+ * Выводит приветственное сообщение:
+ *  <br>
+ *  <em>"Hello, world!"</em>
+ */
 public class HelloWorld {
+    /**
+     * Точка входа  в программу
+     * @param args массив строк, не используется
+     */
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
