210166389a2941beaca5bdcf097f20f7f2793f47
andrey shalagin <andreyshalagin@gmail.com>
2024-07-14 14:34:24 +0300

    Добавлена своя библиотека для печати

--- src/test/java/chapters/three/Binary.java
+++ src/test/java/chapters/three/Binary.java
@@ -1,15 +1,16 @@
 package chapters.three;
 
-import javax.xml.transform.Source;
+import static net.midview.Print.print;
 
 public class Binary {
     public static void main(String[] args) {
         long decimal = 123456789;
         long hex = 0x75BCD15;
         long octal = 0726746425;
-        System.out.printf("Число %s в бинарном виде = %s%n", decimal, Long.toBinaryString(decimal));
-        System.out.printf("16-ричное число %s в бинарном виде = %s%n", hex, Long.toBinaryString(hex));
-        System.out.printf("8-ричное число %s в бинарном виде = %s%n", octal, Long.toBinaryString(octal));
-        System.out.println("Все числа равены между собой: " + (decimal == hex && hex == octal) );
+
+        print(String.format("Число %s в бинарном виде = %s", decimal, Long.toBinaryString(decimal)));
+        print(String.format("16-ричное число %s в бинарном виде = %s", hex, Long.toBinaryString(hex)));
+        print(String.format("8-ричное число %s в бинарном виде = %s", octal, Long.toBinaryString(octal)));
+        print("Все числа равены между собой: " + (decimal == hex && hex == octal));
     }
 }
