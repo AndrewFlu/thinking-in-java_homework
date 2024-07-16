13f149e0a4e3d43f0100662152804e4ac2c4ba41
andrey shalagin <andreyshalagin@gmail.com>
2024-07-16 22:42:08 +0300

    Characters in binary

---
+++ src/test/java/chapters/three/CharsInBinary.java
@@ -0,0 +1,13 @@
+package chapters.three;
+
+import static net.midview.Print.print;
+
+public class CharsInBinary {
+    public static void main(String[] args) {
+        char[] chars = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
+
+        for (Character ch : chars) {
+            print(String.format("'%s' in binary = %s, in decimal = %s", ch, Integer.toBinaryString(ch), Character.getNumericValue(ch)));
+        }
+    }
+}
