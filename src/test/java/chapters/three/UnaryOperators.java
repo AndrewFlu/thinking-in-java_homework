2ccf0afec0c1e3101124f8586d887089bac18a3d
andrey shalagin <andreyshalagin@gmail.com>
2024-07-16 08:30:20 +0300

    Поразрадные операторы: операции - конъюнкции (&) - дезъюнкции (|) - исключающего отрицания (^) - инверсии (~)

--- src/test/java/chapters/three/UnaryOperators.java
+++ src/test/java/chapters/three/UnaryOperators.java
@@ -1,5 +1,6 @@
 package chapters.three;
 
+import static chapters.three.Hexademical.*;
 import static net.midview.Print.print;
 
 public class UnaryOperators {
@@ -13,6 +14,7 @@ public class UnaryOperators {
     public static int EIGHT = 8;
     public static int NINE = 9;
     public static int TEN = 10;
+
 
     public static void main(String[] args) {
         print("One in binary is " + Integer.toBinaryString(ONE));
@@ -41,8 +43,61 @@ public class UnaryOperators {
         print("Поразрядное отрицание [~] (инверсия)");
         System.out.printf("~%s is %s in binary and is %s in decimal%n", TWO, Integer.toBinaryString(~TWO), ~TWO);
         System.out.printf("~%s is %s in binary and is %s in decimal%n", TEN, Integer.toBinaryString(~TEN), ~TEN);
+        print("");
+        print(LOW.toString());
+        print(HIGH.toString());
+        print("");
+        print(HEX_A.toString());
+        print(HEX_F.toString());
+        print(HEX_0.toString());
+        print(HEX_1.toString());
+        print(HEX_7.toString());
+        print("Домашнее задание");
+        print("Коньюнкция [&] (И — поразрядное умножение):");
+        int resultCon = LOW.getValue() & HIGH.getValue();
+        print(String.format("%s & %s = %s. Decimal: %s", LOW.getLiteral(), HIGH.getLiteral(), Integer.toBinaryString(resultCon), resultCon));
+        print("Дезьюнкция [|] (ИЛИ — поразрядное сложение)");
+        int resultDes = LOW.getValue() | HIGH.getValue();
+        print(String.format("%s | %s = %s. Decimal: %s", LOW.getLiteral(), HIGH.getLiteral(), Integer.toBinaryString(resultDes), resultDes));
+        print("Исключающее ИЛИ [^]");
+        int resultXor = LOW.getValue() ^ HIGH.getValue();
+        print(String.format("%s ^ %s = %s. Decimal: %s", LOW.getLiteral(), HIGH.getLiteral(), Integer.toBinaryString(resultXor), resultXor));
+        print("Поразрядное отрицание [~] (инверсия)");
+        int lowInversion = ~LOW.getValue();
+        print(String.format("~%s = %s. Decimal: %s", LOW.getLiteral(), Integer.toBinaryString(lowInversion), lowInversion));
+        int highInversion = ~HIGH.getValue();
+        print(String.format("~%s = %s. Decimal: %s", HIGH.getLiteral(), Integer.toBinaryString(highInversion), highInversion));
 
+    }
+}
 
+enum Hexademical {
+    LOW(0x0001, "0x0001"),
+    HIGH(0x1000, "0x1000"),
+    HEX_0(0x0000, "0x0000"),
+    HEX_1(0x1111, "0x1111"),
+    HEX_7(0x7777, "0x7777"),
+    HEX_A(0xaaaa, "0xaaaa"),
+    HEX_F(0xffff, "0xffff");
 
+    Hexademical(int value, String literal) {
+        this.value = value;
+        this.literal = literal;
+    }
+
+    private final int value;
+    private final String literal;
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getLiteral() {
+        return literal;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s is %s in binary and is %s in decimal", this.getLiteral(), Integer.toBinaryString(this.getValue()), this.getValue());
     }
 }
