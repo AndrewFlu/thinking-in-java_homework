44be94e865342f0fbf989cdf4fba31703f53cd88
andrey shalagin <andreyshalagin@gmail.com>
2024-07-17 07:45:37 +0300

    Logic operations for String

---
+++ src/test/java/chapters/three/StringComparison.java
@@ -0,0 +1,32 @@
+package chapters.three;
+
+import static net.midview.Print.print;
+
+public class StringComparison {
+    private static final String STR_CONST = "123";
+
+    public static void logicOperations(String s1, String s2) {
+        print(String.format("(%s == %s) is %s", s1, s2, s1 == s2));
+        print(String.format("(%s != %s) is %s", s1, s2, s1 != s2));
+//!        print(String.format("(!%s) is %s", s1, !s1));
+//!        print(String.format("(%s < %s) is %s", s1, s2, s1 < s2));
+//!        print(String.format("(%s <= %s) is %s", s1, s2, s1 <= s2));
+//!        print(String.format("(%s > %s) is %s", s1, s2, s1 > s2));
+//!        print(String.format("(%s >= %s) is %s", s1, s2, s1 >= s2));
+        print(String.format("(%s.equals(%s)) is %s", s1, s2, s1.equals(s2)));
+    }
+
+    public static void main(String[] args) {
+        logicOperations("MacBook", "Lenovo");
+        print("");
+        logicOperations("ANDREW", "andrew");
+        print("");
+        logicOperations("andrew", "andrew");
+        print("");
+        logicOperations("", "");
+        print("");
+        logicOperations(STR_CONST, "123");
+        print("");
+        logicOperations( "", null);
+    }
+}
