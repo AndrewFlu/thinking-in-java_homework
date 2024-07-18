556aff136e9783ae5b1b70f5e2eaa8f5c50b5e23
andrey shalagin <andreyshalagin@gmail.com>
2024-07-18 21:58:23 +0300

    Пример использования: brake — прерывает цикл, return — выходит из метода

--- src/test/java/chapters/four/Loops.java
+++ src/test/java/chapters/four/Loops.java
@@ -2,8 +2,12 @@ package chapters.four;
 
 public class Loops {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 104; i++) {
             System.out.println(i);
+            if (i == 99) {
+                break;
+            }
         }
+        System.out.println("break");
     }
 }
