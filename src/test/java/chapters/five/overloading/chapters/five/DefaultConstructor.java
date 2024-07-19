3e8f931cfa3f42e2f1ed3922cfd5b35782e506b5
andrey shalagin <andreyshalagin@gmail.com>
2024-07-19 22:04:25 +0300

    Перегруженный конструктор

--- src/test/java/chapters/five/overloading/chapters/five/DefaultConstructor.java
+++ src/test/java/chapters/five/overloading/chapters/five/DefaultConstructor.java
@@ -3,12 +3,20 @@ package chapters.five.overloading.chapters.five;
 import static net.midview.Print.print;
 
 public class DefaultConstructor {
+
     public DefaultConstructor() {
         print("MacBook is awesome!");
+    }
+
+    public DefaultConstructor(String agree) {
+        this();
+        print(agree);
     }
 }
 class Test {
     public static void main(String[] args) {
         new DefaultConstructor();
+        print("");
+        new DefaultConstructor("Yass!");
     }
 }
