8426360a278ec9756eb3e6bd87f5ca8e2c2ade76
andrey shalagin <andreyshalagin@gmail.com>
2024-07-20 08:39:07 +0300

    Объект с конструктором "По умолчанию"

---
+++ src/test/java/chapters/five/overloading/chapters/five/DefaultSynthesis.java
@@ -0,0 +1,15 @@
+package chapters.five.overloading.chapters.five;
+
+public class DefaultSynthesis {
+    @Override
+    public String toString() {
+        return "DefaultSynthesis{}";
+    }
+}
+
+class TestDrive {
+    public static void main(String[] args) {
+        DefaultSynthesis defaultSynthesis = new DefaultSynthesis();
+        System.out.println(defaultSynthesis);
+    }
+}
