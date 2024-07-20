4ce908664fa28ad7b54575ec579a4579f0d0b33c
andrey shalagin <andreyshalagin@gmail.com>
2024-07-20 08:36:01 +0300

    Перегрузки с разным порядком одинаковых типов аргументов: - конструктора - метода

---
+++ src/test/java/chapters/five/overloading/chapters/five/overloading/SameTypesDifferentOrder.java
@@ -0,0 +1,40 @@
+package chapters.five.overloading.chapters.five.overloading;
+
+import static net.midview.Print.print;
+
+public class SameTypesDifferentOrder {
+    int high;
+    String name;
+
+    public SameTypesDifferentOrder(int high, String name) {
+        this.high = high;
+        this.name = name;
+    }
+
+    public SameTypesDifferentOrder(String name, int high) {
+        this.name = name;
+        this.high = high;
+    }
+
+    static void printName(String prefix, SameTypesDifferentOrder plant) {
+        print(prefix + " " + plant.name);
+    }
+
+    static void printName(SameTypesDifferentOrder plant, String prefix) {
+        print("Перегрузка с другим порядком аргументов: " + prefix + " " + plant.name);
+    }
+
+    public static void main(String[] args) {
+        SameTypesDifferentOrder flower = new SameTypesDifferentOrder(0, "monsterra");
+        SameTypesDifferentOrder tree = new SameTypesDifferentOrder("oukume", 15);
+
+        print(flower.toString());
+        print(tree.toString());
+        print("");
+        printName("Комнатное растение —", flower);
+        printName("Гитары изготавливают из дерева —", tree);
+        print("");
+        printName(flower,"Комнатное растение —");
+        printName(tree, "Гитары изготавливают из дерева —");
+    }
+}
