f3d58e16c4263f5564b1087494d73ade742c5586
andrey shalagin <andreyshalagin@gmail.com>
2024-07-20 11:15:03 +0300

    Демонстрация this для вызова внутренних методов (не делать так)

---
+++ src/test/java/chapters/five/overloading/chapters/five/ThisLink.java
@@ -0,0 +1,19 @@
+package chapters.five.overloading.chapters.five;
+
+public class ThisLink {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.print("Книга \"Игра престолов. Том 1\"");
+    }
+
+    private static class Printer {
+        public void print(String s) {
+            plainPrint(s);
+            this.plainPrint(s);
+        }
+
+        private void plainPrint(String s) {
+            System.out.println(s);
+        }
+    }
+}
