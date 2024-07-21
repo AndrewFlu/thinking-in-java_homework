dd98c6eae53a5cee04d1e5652168c749e5189b15
andrey shalagin <andreyshalagin@gmail.com>
2024-07-21 11:35:24 +0300

    Пример вызова конструктоа из другого конструктора (this())

---
+++ src/test/java/chapters/five/overloading/chapters/five/Book.java
@@ -0,0 +1,27 @@
+package chapters.five.overloading.chapters.five;
+
+import static net.midview.Print.print;
+
+public class Book {
+    public String label;
+
+    public Book(String label) {
+        this.label = label;
+    }
+
+    public Book() {
+        this("Книга без названия");
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book();
+        Book thinkingInJava = new Book("Thinking in Java");
+
+        print(book.getLabel());
+        print(thinkingInJava.getLabel());
+    }
+}
