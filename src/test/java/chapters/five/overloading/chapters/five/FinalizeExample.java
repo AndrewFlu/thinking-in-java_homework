3bbcd3cb3c8a129c786d1bbc09e5c19785471455
andrey shalagin <andreyshalagin@gmail.com>
2024-07-21 11:18:55 +0300

    Пример FinalizeExample

---
+++ src/test/java/chapters/five/overloading/chapters/five/FinalizeExample.java
@@ -0,0 +1,33 @@
+package chapters.five.overloading.chapters.five;
+
+public class FinalizeExample {
+    boolean isReadyToNextGuest;
+
+    public FinalizeExample(boolean isReadyToNextGuest) {
+        this.isReadyToNextGuest = isReadyToNextGuest;
+    }
+
+    public void closeRent() {
+        if (isReadyToNextGuest) {
+            finalize();
+        }
+    }
+
+    @Override
+    public void finalize() {
+            System.out.println("метод finalize() вызван");
+            isReadyToNextGuest = false;
+    }
+
+    public static void main(String[] args) {
+        FinalizeExample ready = new FinalizeExample(true);
+        FinalizeExample notReady = new FinalizeExample(false);
+
+        ready.closeRent();
+        notReady.closeRent();
+    }
+
+    public boolean isReadyToNextGuest() {
+        return isReadyToNextGuest;
+    }
+}
