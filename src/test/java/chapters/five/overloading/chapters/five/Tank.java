7473eadd74bdcc008e7251946057ec6b26465f68
andrey shalagin <andreyshalagin@gmail.com>
2024-07-21 11:26:00 +0300

    Пример применения finalize как проверочного метода перед деструкцией объекта

---
+++ src/test/java/chapters/five/overloading/chapters/five/Tank.java
@@ -0,0 +1,27 @@
+package chapters.five.overloading.chapters.five;
+
+public class Tank {
+    boolean isEmpty;
+
+    public void fill() {
+        isEmpty = false;
+        System.out.println("Сосуд наполнен!");
+    }
+
+    public void empty() {
+        isEmpty = true;
+        System.out.println("Сосуд опустошён");
+    }
+
+    public void finalize() {
+        if (!isEmpty) {
+            System.out.println("Сначала опустошите сосуд");
+        }
+    }
+
+    public static void main(String[] args) {
+        Tank tank = new Tank();
+        tank.fill();
+        tank.finalize();
+    }
+}
