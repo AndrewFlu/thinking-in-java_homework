ea1718e26ea500074127bcb0248e99e27d5c37e1
andrey shalagin <andreyshalagin@gmail.com>
2024-07-22 11:47:28 +0300

    Первый тип инициации массива

--- src/test/java/chapters/five/overloading/chapters/five/StringArray.java
+++ src/test/java/chapters/five/overloading/chapters/five/StringArray.java
@@ -4,7 +4,7 @@ import static net.midview.Print.print;
 
 public class StringArray {
     public static void main(String[] args) {
-        String[] stringArray = new String[]{
+        String[] stringArray = {
                 "MacBook",
                 "is",
                 "awesome",
