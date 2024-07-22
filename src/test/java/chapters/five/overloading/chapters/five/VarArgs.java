2ff32f5e877a40abda814189fb67b37287956d25
andrey shalagin <andreyshalagin@gmail.com>
2024-07-22 22:51:59 +0300

    Метод с аргументом переменной длины

---
+++ src/test/java/chapters/five/overloading/chapters/five/VarArgs.java
@@ -0,0 +1,19 @@
+package chapters.five.overloading.chapters.five;
+
+import static net.midview.Print.print;
+
+public class VarArgs {
+    public void printArgs(String... args) {
+        for (String s : args) {
+            print("argument in method — " + s);
+        }
+    }
+
+    public static void main(String[] args) {
+        VarArgs varArgs = new VarArgs();
+        varArgs.printArgs("MacBook", "is", "awesome", "!");
+        varArgs.printArgs(new String[] {"m1"});
+        varArgs.printArgs(" ");
+        varArgs.printArgs();
+    }
+}
