9d45956a059e631633d74555d4e269a4576674ca
andrey shalagin <andreyshalagin@gmail.com>
2024-07-23 07:55:37 +0300

    Работа с перечислимыми типами

---
+++ src/test/java/chapters/five/overloading/chapters/five/Money.java
@@ -0,0 +1,35 @@
+package chapters.five.overloading.chapters.five;
+
+import static chapters.five.overloading.chapters.five.MoneyType.*;
+import static net.midview.Print.print;
+
+public class Money {
+    private final MoneyType type;
+
+    public Money(MoneyType type) {
+        this.type = type;
+    }
+
+    public void describe() {
+        switch (type) {
+            case METAL -> print(type + " — Металлические монеты");
+            case PAPER -> print(type + " — Бумажные купюры");
+            case DIGIT -> print(type + " — Цифровые деньги");
+            case CRYPTO -> print(type + " — Криптовалюта");
+        }
+    }
+}
+
+class MoneyTestDrive {
+    public static void main(String[] args) {
+        Money metalCoins = new Money(METAL),
+                paper = new Money(PAPER),
+                digit = new Money(DIGIT),
+                crypto = new Money(CRYPTO);
+
+        metalCoins.describe();
+        paper.describe();
+        digit.describe();
+        crypto.describe();
+    }
+}
---
+++ src/test/java/chapters/five/overloading/chapters/five/MoneyType.java
@@ -0,0 +1,9 @@
+package chapters.five.overloading.chapters.five;
+
+public enum MoneyType {
+    METAL,
+    PAPER,
+    DIGIT,
+    CRYPTO
+    ;
+}
