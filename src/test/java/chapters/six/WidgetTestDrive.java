9d1c97d8770d8e2b03d39eb15cb07b5adcb05947
andrey shalagin <andreyshalagin@gmail.com>
2024-07-25 06:57:07 +0300

    Пример наполнения библиотеки access. Класс Widget из этой библиотеки (пакета) доступен извне, т.к. класс объявлен с модификатором public

---
+++ src/test/java/chapters/six/WidgetTestDrive.java
@@ -0,0 +1,9 @@
+package chapters.six;
+
+import chapters.six.access.Widget;
+
+public class WidgetTestDrive {
+    public static void main(String[] args) {
+        new Widget();
+    }
+}
---
+++ src/test/java/chapters/six/access/Widget.java
@@ -0,0 +1,7 @@
+package chapters.six.access;
+
+public class Widget {
+    public Widget() {
+        System.out.println("Конструктор Widget");
+    }
+}
