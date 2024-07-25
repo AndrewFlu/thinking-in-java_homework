6948ed3b14b4031b12d0ad4b06de371d80173853
andrey shalagin <andreyshalagin@gmail.com>
2024-07-25 08:10:49 +0300

    Использование класса за пределами его пакета

---
+++ src/test/java/chapters/six/access/foreign/Foreign.java
@@ -0,0 +1,9 @@
+package chapters.six.access.foreign;
+
+import chapters.six.access.local.PackagedClass;
+
+public class Foreign {
+    public static void main(String[] args) {
+        PackagedClass pc = new PackagedClass();
+    }
+}
---
+++ src/test/java/chapters/six/access/local/PackagedClass.java
@@ -0,0 +1,7 @@
+package chapters.six.access.local;
+
+public class PackagedClass {
+    public PackagedClass() {
+        System.out.println("Создаём класс в пакете");
+    }
+}
