6b90cf5ea533469c85f2c49745224140a00ac6b0
andrey shalagin <andreyshalagin@gmail.com>
2024-07-27 21:30:39 +0300

    Наследование заменено на делегирование

--- src/test/java/chapters/seven/inheritance/Detergent.java
+++ src/test/java/chapters/seven/inheritance/Detergent.java
@@ -1,23 +1,38 @@
 package chapters.seven.inheritance;
 
-public class Detergent extends Cleanser {
+public class Detergent {
+    private final Cleanser cleanser = new Cleanser();
+
     public void scrub() {
-        append(" Переопределённый метод scrub() в Detergent");
-        super.scrub();
+        cleanser.append(" Вызов scrub() делегирован в Cleanser");
+        cleanser.scrub();
     }
 
     public void foam() {
-        append(" foam()");
+        cleanser.append(" foam()");
+    }
+
+    private void dilute() {
+        cleanser.dilute();
+    }
+
+    private void apply() {
+        cleanser.apply();
+    }
+
+    public String toString() {
+        return cleanser.toString();
     }
 
     public static void main(String[] args) {
         Detergent detergent = new Detergent();
+        detergent.scrub();
         detergent.dilute();
         detergent.apply();
-        detergent.scrub();
         detergent.foam();
 
         System.out.println(detergent);
+        System.out.println("");
         System.out.println("А теперь вызовем main() в базовом классе Cleanser");
         Cleanser.main(args);
     }
