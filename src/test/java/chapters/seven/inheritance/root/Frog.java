25520d9b92368cc973eef02e1ec78d2d8e8d3ed8
andrey shalagin <andreyshalagin@gmail.com>
2024-07-28 16:56:08 +0300

    Переопредение методов базового класса

--- src/test/java/chapters/seven/inheritance/root/Frog.java
+++ src/test/java/chapters/seven/inheritance/root/Frog.java
@@ -3,6 +3,15 @@ package chapters.seven.inheritance.root;
 import chapters.seven.inheritance.Amphibian;
 
 public class Frog extends Amphibian {
+
+    public void go() {
+        System.out.println("Прыгать");
+    }
+
+    public void swim() {
+        System.out.println("Плыть по-лягушачьи");
+    }
+
     public static void main(String[] args) {
         Frog frog = new Frog();
 
