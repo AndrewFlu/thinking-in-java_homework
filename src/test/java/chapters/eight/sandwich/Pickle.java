c676eb0d92e3ae2b50bb5d47341ebc4e0e8bdab3
andrey shalagin <andreyshalagin@gmail.com>
2024-07-30 21:31:01 +0300

    Новый класс Pickle в Sandwich

---
+++ src/test/java/chapters/eight/sandwich/Pickle.java
@@ -0,0 +1,7 @@
+package chapters.eight.sandwich;
+
+public class Pickle {
+    public Pickle() {
+        System.out.println("Pickle()");
+    }
+}
--- src/test/java/chapters/eight/sandwich/Sandwich.java
+++ src/test/java/chapters/eight/sandwich/Sandwich.java
@@ -4,6 +4,7 @@ public class Sandwich extends PortableLunch {
     private final Bread bread = new Bread();
     private final Cheese cheese = new Cheese();
     private final Lettuce lettuce = new Lettuce();
+    private final Pickle pickle = new Pickle();
 
     public Sandwich() {
         System.out.println("Sandwich()");
