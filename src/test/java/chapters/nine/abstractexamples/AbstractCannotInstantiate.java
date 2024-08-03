a20658266bf76d0ed79fcc9ac4a48418830d4b95
andrey shalagin <andreyshalagin@gmail.com>
2024-08-03 10:21:08 +0300

    Абстрактный класс не может иметь инстансов

---
+++ src/test/java/chapters/nine/abstractexamples/AbstractCannotInstantiate.java
@@ -0,0 +1,4 @@
+package chapters.nine.abstractexamples;
+
+public abstract class AbstractCannotInstantiate {
+}
---
+++ src/test/java/chapters/nine/abstractexamples/AbstractTestDrive.java
@@ -0,0 +1,9 @@
+package chapters.nine.abstractexamples;
+
+public class AbstractTestDrive {
+    public static void main(String[] args) {
+//        AbstractCannotInstantiate abs = new AbstractCannotInstantiate();
+
+//        System.out.println(abs);
+    }
+}
