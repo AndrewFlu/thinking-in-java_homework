b97b04940bcbc34e867f67be80d6f7754217bd2c
andrey shalagin <andreyshalagin@gmail.com>
2024-08-08 17:50:00 +0300

    Демо того, что внутренний класс имеет доступ к полям и методам внешнего класса

--- src/test/java/chapters/ten/innerreference/InnerTestDrive.java
+++ src/test/java/chapters/ten/innerreference/InnerTestDrive.java
@@ -2,7 +2,10 @@ package chapters.ten.innerreference;
 
 public class InnerTestDrive {
     public static void main(String[] args) {
-        Outer.Inner inner = new Outer().getInner();
-        System.out.println(inner.getClass().getSimpleName());
+        Outer.Inner inner = new Outer("Outer class").getInner();
+        Outer.Inner inner2 = new Outer("Outer class 2").getInner();
+
+        System.out.println(inner.toString());
+        System.out.println(inner2);
     }
 }
--- src/test/java/chapters/ten/innerreference/Outer.java
+++ src/test/java/chapters/ten/innerreference/Outer.java
@@ -1,12 +1,22 @@
 package chapters.ten.innerreference;
 
 public class Outer {
+    private String info;
+
+    public Outer(String info) {
+        this.info = info;
+    }
+
     public Inner getInner() {
         return new Inner();
     }
     class Inner {
         public Inner() {
             System.out.println("Inner class has been created");
+        }
+
+        public String toString() {
+            return info;
         }
     }
 }
