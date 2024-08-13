8b3a9fa27cb9515fc4cf56cc09516d4bcfefb88d
andrey shalagin <andreyshalagin@gmail.com>
2024-08-13 07:50:10 +0300

    Пример внутреннего класса, размещённого в локальной области действия (внутри метода)

---
+++ src/test/java/chapters/ten/locals/ISum.java
@@ -0,0 +1,5 @@
+package chapters.ten.locals;
+
+public interface ISum {
+    int addition(int a, int b);
+}
---
+++ src/test/java/chapters/ten/locals/Mathematical.java
@@ -0,0 +1,26 @@
+package chapters.ten.locals;
+
+public class Mathematical {
+    ISum sum() {
+        class LocalSum implements ISum {
+
+            @Override
+            public int addition(int a, int b) {
+                return a + b;
+            }
+        }
+
+        return new LocalSum();
+    }
+
+    public static void main(String[] args) {
+        Mathematical math = new Mathematical();
+        ISum sum = math.sum();
+        int i = 2;
+        int j = 5;
+        System.out.println(String.format("%s + %s = %s", i, j, sum.addition(i, j)));
+        i = -5;
+        j = 10;
+        System.out.println(String.format("%s + %s = %s", i, j, sum.addition(i, j)));
+    }
+}
