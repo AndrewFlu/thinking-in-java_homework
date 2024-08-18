7c58a23de8f6b6763dbfad3457bffe5abb68a5d7
andrey shalagin <andreyshalagin@gmail.com>
2024-08-18 09:03:00 +0300

    Реализация SimpleCollection через ArrayList

---
+++ src/test/java/chapters/eleven/containers/simplecollection/SimpleCollection.java
@@ -0,0 +1,17 @@
+package chapters.eleven.containers.simplecollection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class SimpleCollection {
+    public static void main(String[] args) {
+        Collection<Integer> c = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            c.add(i); // автоматическая упаковка
+        }
+        for (Integer i : c) {
+            System.out.print(i + ", ");
+        }
+
+    }
+}
