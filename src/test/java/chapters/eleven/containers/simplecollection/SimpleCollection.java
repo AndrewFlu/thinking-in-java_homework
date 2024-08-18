60e4818e1034ef987d9267a00247b3cade8b03ed
andrey shalagin <andreyshalagin@gmail.com>
2024-08-18 09:04:37 +0300

    Реализация SimpleCollection через HashSet

--- src/test/java/chapters/eleven/containers/simplecollection/SimpleCollection.java
+++ src/test/java/chapters/eleven/containers/simplecollection/SimpleCollection.java
@@ -1,11 +1,11 @@
 package chapters.eleven.containers.simplecollection;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class SimpleCollection {
     public static void main(String[] args) {
-        Collection<Integer> c = new ArrayList<>();
+        Collection<Integer> c = new HashSet<>();
         for (int i = 0; i < 10; i++) {
             c.add(i); // автоматическая упаковка
         }
