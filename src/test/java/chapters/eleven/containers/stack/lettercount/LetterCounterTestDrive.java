2244341aec687d17baa330d98b733582729910be
andrey shalagin <andreyshalagin@gmail.com>
2024-08-24 16:02:20 +0300

    Подсчёт количества гласных в файле

--- src/test/java/chapters/eleven/containers/stack/lettercount/LetterCounterTestDrive.java
+++ src/test/java/chapters/eleven/containers/stack/lettercount/LetterCounterTestDrive.java
@@ -10,7 +10,8 @@ public class LetterCounterTestDrive {
     public static void main(String[] args) {
         String word;
         Set<String> wordsInFile = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
-        wordsInFile.addAll(new TextFile("src/test/java/chapters/eleven/containers/stack/StackTestDrive.java", "\\W+"));
+        TextFile file = new TextFile("src/test/java/chapters/eleven/containers/stack/StackTestDrive.java", "\\W+");
+        wordsInFile.addAll(file);
 
         Iterator<String> iterator = wordsInFile.iterator();
         while (iterator.hasNext()) {
@@ -18,5 +19,18 @@ public class LetterCounterTestDrive {
             System.out.printf("В слове %s количество гласных букв = %s%n", word, LetterCounter.getVowelsCount(word));
         }
 
+        System.out.println();
+        int allVowelsCount = getSumOfVowels(file);
+        System.out.println("Общее количество гласных в файле = " + allVowelsCount);
+
+    }
+
+    private static int getSumOfVowels(TextFile file) {
+        int sum = 0;
+        for (String s : file) {
+            sum += LetterCounter.getVowelsCount(s);
+        }
+
+        return sum;
     }
 }
