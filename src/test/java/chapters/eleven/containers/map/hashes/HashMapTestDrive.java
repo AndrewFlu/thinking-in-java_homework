82947e91cd02b2c9007ec1e1264c8a1de60dcd1d
andrey shalagin <andreyshalagin@gmail.com>
2024-08-25 08:20:16 +0300

    Добавлены HashSet и LinkedHashSet

--- src/test/java/chapters/eleven/containers/map/hashes/HashMapTestDrive.java
+++ src/test/java/chapters/eleven/containers/map/hashes/HashMapTestDrive.java
@@ -13,14 +13,23 @@ public class HashMapTestDrive {
         smeta.put("дверь", 40000);
         smeta.put("мебель", 100000);
 
+        System.out.println("Initial Map:");
         System.out.println(smeta);
         Set<String> keySet = smeta.keySet();
         Set<String> sortedSet = new TreeSet<>(keySet);
 
+        System.out.println("LinkedHashMap: ");
         Map<String, Integer> sorted = new LinkedHashMap<>();
         for (String s : sortedSet) {
             sorted.put(s, smeta.get(s));
         }
         System.out.println(sorted);
+
+        System.out.println("HashSet:");
+        System.out.println(keySet);
+
+        System.out.println("LinkedHashSet:");
+        Set<String> linkedHashSet = new LinkedHashSet<>(sortedSet);
+        System.out.println(linkedHashSet);
     }
 }
