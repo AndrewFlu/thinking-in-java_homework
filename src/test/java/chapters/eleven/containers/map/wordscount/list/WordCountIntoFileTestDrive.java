c95c03b71931849c62e285af9b5ddafbb2765fa2
andrey shalagin <andreyshalagin@gmail.com>
2024-08-26 13:20:47 +0300

    Подсчёт количетсва повтроений слов в файле с выводом всех позиций, в которых встречается данное слово

---
+++ src/test/java/chapters/eleven/containers/map/wordscount/list/WordCountIntoFileTestDrive.java
@@ -0,0 +1,36 @@
+package chapters.eleven.containers.map.wordscount.list;
+
+import chapters.eleven.containers.stack.TextFile;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCountIntoFileTestDrive {
+    public static void main(String[] args) {
+        TextFile file = new TextFile("src/test/java/chapters/eleven/containers/map/wordscount/UniqueWordsAlphabetic.java", "\\W+");
+        Map<String, ArrayList<Integer>> wordFrequency = new HashMap<>();
+
+        for (int i = 0; i < file.size(); i++) {
+            String currentKey = file.get(i);
+            if (wordFrequency.containsKey(currentKey)) {
+                ArrayList<Integer> currentList = wordFrequency.get(currentKey);
+                currentList.add(i);
+            } else {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(i);
+                wordFrequency.put(currentKey, list);
+            }
+        }
+
+        printFrequency(wordFrequency);
+    }
+
+    private static void printFrequency(Map<String, ArrayList<Integer>> wordFrequency) {
+        for (String key : wordFrequency.keySet()) {
+            ArrayList<Integer> value = wordFrequency.get(key);
+            System.out.println(String.format("Слово [%s] встречается в файле %s раз(-а)", key, value.size()));
+            System.out.println(String.format("    в позициях: %s", value));
+        }
+    }
+}
