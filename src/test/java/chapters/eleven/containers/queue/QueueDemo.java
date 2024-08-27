e3e7366a91dab6d331a9099a284367c64516ef5a
andrey shalagin <andreyshalagin@gmail.com>
2024-08-27 07:43:07 +0300

    Пример работы с Очередью (Queue)

---
+++ src/test/java/chapters/eleven/containers/queue/QueueDemo.java
@@ -0,0 +1,30 @@
+package chapters.eleven.containers.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            queue.offer(random.nextInt(i + 10));
+        }
+        printQ(queue);
+
+        Queue<Character> qc = new LinkedList<>();
+        for (Character c : "MacBook".toCharArray()) {
+            qc.offer(c);
+        }
+        printQ(qc);
+
+    }
+
+    private static void printQ(Queue<?> queue) {
+        while (queue.peek() != null) {
+            System.out.print(queue.remove() + " ");
+        }
+        System.out.println();
+    }
+}
