5266722ec569866b18522f3bf7c0842e7c421b20
andrey shalagin <andreyshalagin@gmail.com>
2024-08-31 08:01:40 +0300

    Имплементация Iterable для NonCollectionSequence. Реализация методов reversed() и randomized()

--- src/test/java/chapters/eleven/containers/iterators/noncollection/NonCollectionSequence.java
+++ src/test/java/chapters/eleven/containers/iterators/noncollection/NonCollectionSequence.java
@@ -3,10 +3,9 @@ package chapters.eleven.containers.iterators.noncollection;
 import chapters.eleven.containers.collectionanditerator.pets.Pet;
 import chapters.eleven.containers.collectionanditerator.pets.Pets;
 
-import java.util.Iterator;
+import java.util.*;
 
-public class NonCollectionSequence extends PetSequence {
-
+public class NonCollectionSequence extends PetSequence implements Iterable<Pet> {
 
     public Iterator<Pet> iterator() {
         return new Iterator<Pet>() {
@@ -20,6 +19,42 @@ public class NonCollectionSequence extends PetSequence {
             @Override
             public Pet next() {
                 return pets[index++];
+            }
+        };
+    }
+
+    public Iterable<Pet> reversed() {
+        return new Iterable<Pet>() {
+            private int index = pets.length - 1;
+
+            @Override
+            public Iterator<Pet> iterator() {
+                return new Iterator<Pet>() {
+                    @Override
+                    public boolean hasNext() {
+                        return index > -1;
+                    }
+
+                    @Override
+                    public Pet next() {
+                        return pets[index--];
+                    }
+                };
+            }
+        };
+    }
+
+    public Iterable<Pet> randomized() {
+        return new Iterable<Pet>() {
+
+            private final Random random = new Random();
+
+            @Override
+            public Iterator<Pet> iterator() {
+                var arrayList = new ArrayList<>(Arrays.asList(pets));
+                Collections.shuffle(arrayList, random);
+
+                return arrayList.iterator();
             }
         };
     }
--- src/test/java/chapters/eleven/containers/iterators/noncollection/NonCollectionSequenceTestDrive.java
+++ src/test/java/chapters/eleven/containers/iterators/noncollection/NonCollectionSequenceTestDrive.java
@@ -8,6 +8,20 @@ public class NonCollectionSequenceTestDrive {
     public static void main(String[] args) {
         NonCollectionSequence nc = new NonCollectionSequence();
         printQ(nc.iterator());
+
+        System.out.println("Initial array:");
+        printQ(nc.iterator());
+
+        System.out.println("reversed:");
+        printQ(nc.reversed().iterator());
+
+        System.out.println("randomized:");
+        for (Pet pet : nc.randomized()) {
+            System.out.println(pet.getName());
+        }
+
+        System.out.println("Initial array:");
+        printQ(nc.iterator());
     }
 
     private static void printQ(Iterator<Pet> iterator) {
