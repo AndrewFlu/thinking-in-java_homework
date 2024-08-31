ffaec6295bdecb54512b71711fbd3bef4937a93c
andrey shalagin <andreyshalagin@gmail.com>
2024-08-31 21:48:34 +0300

    Задание 3. Перехват ArrayIndexOutOfBoundException

---
+++ src/test/java/chapters/twelve/exceptions/IndexExceptionTestDrive.java
@@ -0,0 +1,14 @@
+package chapters.twelve.exceptions;
+
+public class IndexExceptionTestDrive {
+    public static void main(String[] args) {
+        try {
+            Integer[] ints = {1, 2, 3, 4, 5};
+            Integer outOfBound = ints[5];
+            System.out.println(outOfBound);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println(ex.getLocalizedMessage());
+        }
+
+    }
+}
