cc43cbbfc9aae23ee13d9b2b52b20d5353a60378
andrey shalagin <andreyshalagin@gmail.com>
2024-09-01 08:50:04 +0300

    Задание 9. Система диагностики автомобиля: работа с тремя типами исключений

---
+++ src/test/java/chapters/twelve/exceptions/multiexception/IgnitionException.java
@@ -0,0 +1,11 @@
+package chapters.twelve.exceptions.multiexception;
+
+public class IgnitionException extends Exception {
+    private static final int CODE = 1142;
+    private static final String DESCRIPTION = "Ошибка зажигания. Неисправность катушки зажигания";
+
+    @Override
+    public String getMessage() {
+        return "Код ошибки — " + CODE + ": " + DESCRIPTION;
+    }
+}
