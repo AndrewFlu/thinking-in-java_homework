ce7f1422e75f649d8cb27ccddf2eca3d1452f09b
andrey shalagin <andreyshalagin@gmail.com>
2024-09-01 16:08:41 +0300

    Задание 15. Пример корректной работы программы в случае выброс RuntimeException благодаря действиям в блоке finally

--- src/test/java/chapters/twelve/exceptions/finallyblock/WithFinally.java
+++ src/test/java/chapters/twelve/exceptions/finallyblock/WithFinally.java
@@ -4,14 +4,16 @@ public class WithFinally {
     static Switch switcher = new Switch();
 
     public static void main(String[] args) {
-        switcher.on();
-        // код, способный возбуждать исключения
         try {
+            switcher.on();
+            // код, способный возбуждать исключения
             OnOffSwitch.f();
         } catch (OnOffException1 e) {
-            throw new RuntimeException(e);
+            System.out.println("OnOffException1");
         } catch (OnOffException2 e) {
-            throw new RuntimeException(e);
+            System.out.println("OnOffException2");
+        } finally {
+            switcher.off();
         }
     }
 }
