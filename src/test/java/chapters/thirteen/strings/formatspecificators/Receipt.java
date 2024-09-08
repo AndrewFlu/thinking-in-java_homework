2b1a9a20a8ce14755fce3ca2ef66b5823620fa3f
andrey shalagin <andreyshalagin@gmail.com>
2024-09-08 20:52:53 +0300

    Рефакторинг: размеры границ - в константы

--- src/test/java/chapters/thirteen/strings/formatspecificators/Receipt.java
+++ src/test/java/chapters/thirteen/strings/formatspecificators/Receipt.java
@@ -3,23 +3,27 @@ package chapters.thirteen.strings.formatspecificators;
 import java.util.Formatter;
 
 public class Receipt {
+    public static final String CENTER_WIDTH = "5";
+    public static final String LEFT_BORDER_WIDTH = "1" + CENTER_WIDTH;
+    public static final String RIGHT_BORDER_WIDTH = "10";
+
+    private final Formatter f = new Formatter(System.out);
     private double total = 0;
-    private Formatter f = new Formatter(System.out);
 
     public void printTitle() {
-        f.format("%-15s %5s %10s\n", "Item", "Qty", "Price");
-        f.format("%-15s %5s %10s\n", "----", "---", "-----");
+        f.format("%-" + LEFT_BORDER_WIDTH + "s %" + CENTER_WIDTH + "s %" + RIGHT_BORDER_WIDTH + "s\n", "Item", "Qty", "Price");
+        f.format("%-" + LEFT_BORDER_WIDTH + "s %" + CENTER_WIDTH + "s %" + RIGHT_BORDER_WIDTH + "s\n", "----", "---", "-----");
     }
 
     public void print(String name, int qty, double price) {
-        f.format("%-15.15s %5d %10.2f\n", name, qty, price);
+        f.format("%-" + LEFT_BORDER_WIDTH + ".1" + CENTER_WIDTH + "s %5d %" + RIGHT_BORDER_WIDTH + ".2f\n", name, qty, price);
         total += price;
     }
 
     public void printTotal() {
-        f.format("%-15s %5s %10.2f\n", "Tax", "", total * 0.06);
-        f.format("%-15s %5s %10s\n", "", "", "-----");
-        f.format("%-15s %5s %10.2f\n", "Total", "", total * 1.06);
+        f.format("%-" + LEFT_BORDER_WIDTH + "s %" + CENTER_WIDTH + "s %" + RIGHT_BORDER_WIDTH + ".2f\n", "Tax", "", total * 0.06);
+        f.format("%-" + LEFT_BORDER_WIDTH + "s %" + CENTER_WIDTH + "s %" + RIGHT_BORDER_WIDTH + "s\n", "", "", "-----");
+        f.format("%-" + LEFT_BORDER_WIDTH + "s %" + CENTER_WIDTH + "s %" + RIGHT_BORDER_WIDTH + ".2f\n", "Total", "", total * 1.06);
     }
 
     public static void main(String[] args) {
