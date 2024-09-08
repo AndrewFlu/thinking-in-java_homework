fe85b9ee7b903958e01bf5d36db6d1e0b075cd06
andrey shalagin <andreyshalagin@gmail.com>
2024-09-08 20:00:17 +0300

    Infinite recursion

---
+++ src/test/java/chapters/thirteen/strings/recursion/InfiniteRecursion.java
@@ -0,0 +1,20 @@
+package chapters.thirteen.strings.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfiniteRecursion {
+    @Override
+    public String toString() {
+        return "Address of class InfiniteRecursion is: " + this;
+    }
+
+    public static void main(String[] args) {
+        List<InfiniteRecursion> recursions = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            recursions.add(new InfiniteRecursion());
+        }
+
+        System.out.println(recursions);
+    }
+}
