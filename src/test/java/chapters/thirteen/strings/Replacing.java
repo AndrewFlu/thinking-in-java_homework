0765cdb5407fa03931c0730a32e199c6679545ef
andrey shalagin <andreyshalagin@gmail.com>
2024-09-13 22:50:09 +0300

    Replacing.java

---
+++ src/test/java/chapters/thirteen/strings/Replacing.java
@@ -0,0 +1,12 @@
+package chapters.thirteen.strings;
+
+import static net.midview.Print.print;
+
+public class Replacing {
+    static String s = Splitting.knights;
+
+    public static void main(String[] args) {
+        print(s.replaceFirst("f\\w+", "located"));
+        print(s.replaceAll("strawberry|tree|heering", "banana"));
+    }
+}
