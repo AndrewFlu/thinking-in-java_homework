24e0d1f71acb45a8962e6041139d14c7658a5166
andrey shalagin <andreyshalagin@gmail.com>
2024-09-17 07:50:52 +0300

    StartEnd example

---
+++ src/test/java/chapters/thirteen/strings/startend/StartEnd.java
@@ -0,0 +1,58 @@
+package chapters.thirteen.strings.startend;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static net.midview.Print.print;
+
+public class StartEnd {
+    public static String input = "As long as there is injustice, whenever a\n" +
+            "Targathian baby cries out, wherever a distress\n" +
+            "signal sounds among the stars ... We'll be there.\n" +
+            "This fine ship, and this fine crew ...\n" +
+            "Never give up! Never surrender!";
+
+    static void examine(String s, String regex) {
+        Display d = new Display(regex);
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(s);
+
+        while (m.find()) {
+            d.display("find() '" + m.group() + "' start = " + m.start() + " end = " + m.end());
+            if (m.lookingAt()) { // вызов reset() не нужен
+                d.display("lookingAt() start = " + m.start() + " end = " + m.end());
+            }
+            if (m.matches()) {  // вызов reset() не нужен
+                d.display("matches() start = " + m.start() + " end = " + m.end());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        for (String in : StartEnd.input.split("\n")) {
+            print("input: " + in);
+            for (String regex : new String[]{"\\w*ere\\w*", "T\\w+", "Never.*?!"}) {
+                StartEnd.examine(in, regex);
+            }
+        }
+    }
+
+    private static class Display {
+        private boolean regexPrinted = false;
+        private final String regex;
+
+        Display(String regex) {
+            this.regex = regex;
+        }
+
+        void display(String message) {
+            if (!regexPrinted) {
+                print(regex);
+                regexPrinted = true;
+            }
+
+            print(message);
+        }
+    }
+
+}
