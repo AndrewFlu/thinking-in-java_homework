61f9e069ba670fa9c0e421f640fa45648c474798
andrey shalagin <andreyshalagin@gmail.com>
2024-09-20 08:42:10 +0300

    Задание 16: поддержка обработки всех файлов в каталоге

--- src/test/java/chapters/thirteen/strings/grep/JGrep.java
+++ src/test/java/chapters/thirteen/strings/grep/JGrep.java
@@ -2,13 +2,16 @@ package chapters.thirteen.strings.grep;
 
 import utils.TextFile;
 
+import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JGrep {
     private static Map<String, Integer> flagsMap = new HashMap<>();
+
     static {
         flagsMap.put("MULTILINE", Pattern.MULTILINE);
         flagsMap.put("CASE_INSENSITIVE", Pattern.CASE_INSENSITIVE);
@@ -21,14 +24,26 @@ public class JGrep {
         }
 
         Pattern p = Pattern.compile(args[1], flagsMap.get(args[2]));
-        // перебор строк входного файла
-        int index = 0;
         Matcher m = p.matcher("");
-        for (String line : new TextFile(args[0])) {
-            m.reset(line);
-            while (m.find()) {
+
+        File file = new File(args[0]);
+        if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                processFile(f, m);
+            }
+        } else {
+            processFile(file, m);
+        }
+    }
+
+    private static void processFile(File file, Matcher matcher) {
+        int index = 0;
+        List<String> strings = new TextFile(file.getPath());
+        for (String line : strings) {
+            matcher.reset(line);
+            while (matcher.find()) {
                 System.out.println(index++ + ": " +
-                        m.group() + ": " + m.start());
+                        matcher.group() + ": " + matcher.start());
             }
         }
     }
