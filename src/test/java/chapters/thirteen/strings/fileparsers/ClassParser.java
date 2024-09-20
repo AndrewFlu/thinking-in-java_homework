39ba6fd94ae5317a65dc219081ecdb1c563a626b
andrey shalagin <andreyshalagin@gmail.com>
2024-09-20 22:18:48 +0300

    Задание 19: программа, выводящая все внешние классы, использованные в файле

---
+++ src/test/java/chapters/thirteen/strings/fileparsers/ClassParser.java
@@ -0,0 +1,22 @@
+package chapters.thirteen.strings.fileparsers;
+
+import utils.TextFile;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ClassParser {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: <fileName.extension>");
+            System.exit(0);
+        }
+
+        Pattern p = Pattern.compile("(\\b[A-Z][A-z]+(?)\\b);$", Pattern.MULTILINE);
+        Matcher m = p.matcher(TextFile.read(args[0]));
+        System.out.println("В классе " + ClassParser.class.getSimpleName() + " используются следующие классы:");
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+    }
+}
