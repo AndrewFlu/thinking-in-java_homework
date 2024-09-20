3ffd96c5940d34b87eee5ae51f810efdd2a7b9c0
andrey shalagin <andreyshalagin@gmail.com>
2024-09-20 08:35:51 +0300

    Задание 15: поддержка флагов для JGrep.java

--- src/test/java/chapters/thirteen/strings/grep/JGrep.java
+++ src/test/java/chapters/thirteen/strings/grep/JGrep.java
@@ -2,17 +2,25 @@ package chapters.thirteen.strings.grep;
 
 import utils.TextFile;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JGrep {
+    private static Map<String, Integer> flagsMap = new HashMap<>();
+    static {
+        flagsMap.put("MULTILINE", Pattern.MULTILINE);
+        flagsMap.put("CASE_INSENSITIVE", Pattern.CASE_INSENSITIVE);
+    }
+
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: <filename.extension> <regular expression>");
+        if (args.length < 3) {
+            System.out.println("Usage: <filename.extension> <regular expression>, Pattern.<flag>");
             System.exit(0);
         }
 
-        Pattern p = Pattern.compile(args[1]);
+        Pattern p = Pattern.compile(args[1], flagsMap.get(args[2]));
         // перебор строк входного файла
         int index = 0;
         Matcher m = p.matcher("");
