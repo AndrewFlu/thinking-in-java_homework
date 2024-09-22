593315e8be0584c07bf7f278aee2e43752611873
andrey shalagin <andreyshalagin@gmail.com>
2024-09-22 09:59:53 +0300

    Задание 2: Новый интерфейс в FancyToy

--- src/test/java/chapters/forteen/typeinfo/toys/FancyToy.java
+++ src/test/java/chapters/forteen/typeinfo/toys/FancyToy.java
@@ -1,6 +1,6 @@
 package chapters.forteen.typeinfo.toys;
 
-public class FancyToy extends Toy implements HasBatteries, WaterProof, Shoots {
+public class FancyToy extends Toy implements HasBatteries, WaterProof, Shoots, PlayMusic {
     public FancyToy() {
         super(1);
     }
