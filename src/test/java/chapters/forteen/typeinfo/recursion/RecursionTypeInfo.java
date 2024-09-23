596c5bebe6c6386bf7929cc838971f2d420c32bd
andrey shalagin <andreyshalagin@gmail.com>
2024-09-23 22:37:26 +0300

    Задание 9: вывод информации о полях класса в иерархии

--- src/test/java/chapters/forteen/typeinfo/recursion/RecursionTypeInfo.java
+++ src/test/java/chapters/forteen/typeinfo/recursion/RecursionTypeInfo.java
@@ -2,6 +2,10 @@ package chapters.forteen.typeinfo.recursion;
 
 import chapters.forteen.typeinfo.shapes.Triangle;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
 import static net.midview.Print.print;
 
 public class RecursionTypeInfo {
@@ -12,7 +16,17 @@ public class RecursionTypeInfo {
     private static void printHierarchyInfo(Class clazz) {
         System.out.printf("Класс [%s] имеет следующую иерархию: \n", clazz.getSimpleName());
         while (clazz != null) {
-            print("Класс: " + clazz.getName());
+            print("\tКласс: " + clazz.getName() + " ");
+            if (clazz.getDeclaredFields().length > 0) {
+                print("\tКласс: " + clazz.getName() + " имеет следующие поля:");
+                List<String> collect = Arrays.stream(clazz.getDeclaredFields())
+                        .map(Field::getName)
+                        .toList();
+                for (String s : collect) {
+                    System.out.println("\t\t" + s);
+                }
+            }
+
             clazz = clazz.getSuperclass();
         }
     }
