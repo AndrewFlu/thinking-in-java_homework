f5fe4b85fa3ce948019fa047166e83907cfefae6
andrey shalagin <andreyshalagin@gmail.com>
2024-09-29 06:54:59 +0300

    Рекурсивный подсчёт типов с помощью универсального класса TypeCounter

---
+++ src/test/java/chapters/forteen/typeinfo/pets/PetCount4.java
@@ -0,0 +1,15 @@
+package chapters.forteen.typeinfo.pets;
+
+import utils.TypeCounter;
+
+public class PetCount4 {
+    public static void main(String[] args) {
+        TypeCounter counter = new TypeCounter(Pet.class);
+        for (Pet pet : Pets.createArray(20)) {
+            System.out.print(pet.getClass().getSimpleName() + " ");
+            counter.count(pet);
+        }
+        System.out.println();
+        System.out.println(counter);
+    }
+}
