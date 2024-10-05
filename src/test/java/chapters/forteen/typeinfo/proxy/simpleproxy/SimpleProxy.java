58817182132d09f33c415d15154964639986e7a0
andrey shalagin <andreyshalagin@gmail.com>
2024-10-05 21:54:49 +0300

    Задание 21: Фиксация даты-времени (DateTime.now()) вызова методов через proxy-объект

--- src/test/java/chapters/forteen/typeinfo/proxy/simpleproxy/SimpleProxy.java
+++ src/test/java/chapters/forteen/typeinfo/proxy/simpleproxy/SimpleProxy.java
@@ -1,5 +1,10 @@
 package chapters.forteen.typeinfo.proxy.simpleproxy;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+
 import static net.midview.Print.print;
 
 public class SimpleProxy implements Interface {
@@ -12,13 +17,13 @@ public class SimpleProxy implements Interface {
     @Override
     public void doSomething() {
         // дополнительное поведение, наделяемое proxy-объекту
-        print("SimpleProxy do something...");
+        print("Метод Interface.doSomething() вызван " + LocalDateTime.now());
         proxied.doSomething();
     }
 
     @Override
     public void somethingElse(String arg) {
-        print("SimpleProxy something else... " + arg);
+        print("Метод Interface.somethingElse(String arg) вызван " + LocalDateTime.now());
         proxied.somethingElse(arg);
     }
 }
