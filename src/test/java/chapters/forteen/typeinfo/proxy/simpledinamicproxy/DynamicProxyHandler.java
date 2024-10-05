e74bc0784bc794b2607788117b2c4221ae9c0e4f
andrey shalagin <andreyshalagin@gmail.com>
2024-10-05 21:58:15 +0300

    Задание 22: Фиксация даты-времени (LocalTime.now()) вызова методов через dynamic-proxy-объект

--- src/test/java/chapters/forteen/typeinfo/proxy/simpledinamicproxy/DynamicProxyHandler.java
+++ src/test/java/chapters/forteen/typeinfo/proxy/simpledinamicproxy/DynamicProxyHandler.java
@@ -2,6 +2,9 @@ package chapters.forteen.typeinfo.proxy.simpledinamicproxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.time.LocalTime;
+
+import static net.midview.Print.print;
 
 public class DynamicProxyHandler implements InvocationHandler {
     private Object proxied;
@@ -12,13 +15,14 @@ public class DynamicProxyHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println("*** proxy: " + proxy.getClass() +
-                ", method: " + method + ", args: " + args);
-        if (args != null) {
-            for (Object arg : args) {
-                System.out.println(" " + arg);
-            }
-        }
+//        System.out.println("*** proxy: " + proxy.getClass() +
+//                ", method: " + method + ", args: " + args);
+//        if (args != null) {
+//            for (Object arg : args) {
+//                System.out.println(" " + arg);
+//            }
+//        }
+        print("Время вызова метода [" + method.getName() + "]: " + LocalTime.now());
 
         return method.invoke(proxied, args);
     }
