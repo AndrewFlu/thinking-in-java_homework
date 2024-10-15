a67b17acb0c5a248dabe9e2c6aa94d082acb89de
andrey shalagin <andreyshalagin@gmail.com>
2024-10-15 07:26:15 +0300

    Пример реализации простого стека

---
+++ src/test/java/chapters/fifteen/generics/stack/LinkedStack.java
@@ -0,0 +1,52 @@
+package chapters.fifteen.generics.stack;
+
+public class LinkedStack<T> {
+    private static class Node<U> {
+        U item;
+        Node<U> next;
+
+        Node() {
+            item = null;
+            next = null;
+        }
+
+        Node(U item, Node<U> next) {
+            this.item = item;
+            this.next = next;
+        }
+
+        boolean end() {
+            return item == null && next == null;
+        }
+    }
+
+    // fields
+    // sentinel — сторож
+    private Node<T> top = new Node<T>();
+
+    public void push(T item) {
+        top = new Node(item, top);
+    }
+
+    public T pop() {
+        T result = top.item;
+        if (!top.end()) {
+            top = top.next;
+        }
+
+        return result;
+    }
+
+    // test-drive
+    public static void main(String[] args) {
+        LinkedStack<String> lss = new LinkedStack<>();
+        for (String s : "MacBook is Awesome!".split("\\s")) {
+            lss.push(s);
+        }
+
+        String s;
+        while((s = lss.pop()) != null) {
+            System.out.println(s);
+        }
+    }
+}
