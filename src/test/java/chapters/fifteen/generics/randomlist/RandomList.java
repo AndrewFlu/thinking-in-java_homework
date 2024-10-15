da94764f7335888693e79fddb9587f7bacaf7a23
andrey shalagin <andreyshalagin@gmail.com>
2024-10-15 07:45:03 +0300

    Задание 6: RandomList с тремя разными типами

--- src/test/java/chapters/fifteen/generics/randomlist/RandomList.java
+++ src/test/java/chapters/fifteen/generics/randomlist/RandomList.java
@@ -1,5 +1,8 @@
 package chapters.fifteen.generics.randomlist;
 
+import chapters.forteen.typeinfo.pets.Pet;
+import chapters.forteen.typeinfo.pets.Pets;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -17,6 +20,7 @@ public class RandomList<T> {
 
     // test-drive
     public static void main(String[] args) {
+        // string
         RandomList<String> rls = new RandomList<>();
         for (String s : ("The quick brown fox jumped over the lazy brown dog".split("\\s"))) {
             rls.add(s);
@@ -24,6 +28,30 @@ public class RandomList<T> {
 
         for (int i = 0; i < 15; i++) {
             System.out.print(rls.select() + " ");
+        }
+
+        System.out.println();
+
+        // integer
+        RandomList<Integer> randomInts = new RandomList<Integer>();
+        for (int i = 0; i < 50; i++) {
+            randomInts.add(i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(randomInts.select() + " ");
+        }
+        System.out.println();
+
+        // pets
+        RandomList<Pet> randomPets = new RandomList<Pet>();
+        Pet[] array = Pets.createArray(10);
+        for (Pet pet : array) {
+            randomPets.add(pet);
+        }
+
+        for (int i = 0; i < 30; i++) {
+            System.out.print(randomPets.select() + " ");
         }
     }
 }
