060a72e3ac9ba44ce66dcc126434c04397f0cde4
andrey shalagin <andreyshalagin@gmail.com>
2024-10-16 07:48:49 +0300

    Реализация паттерна "Адаптер" для поддержки Iterable программы для получения чисел Фибоначчи

---
+++ src/test/java/chapters/fifteen/generics/fibonacci/IterableFibonacci.java
@@ -0,0 +1,39 @@
+package chapters.fifteen.generics.fibonacci;
+
+import java.util.Iterator;
+
+public class IterableFibonacci extends Fibonacci implements Iterable<Integer>{
+    private int n;
+
+    public IterableFibonacci(int count) {
+        this.n = count;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return n > 0;
+            }
+
+            @Override
+            public Integer next() {
+                n--;
+                return IterableFibonacci.this.next();
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    // test-drive
+        public static void main(String[] args) {
+            for (int i : new IterableFibonacci(18)) {
+                System.out.print(i + " ");
+            }
+        }
+}
