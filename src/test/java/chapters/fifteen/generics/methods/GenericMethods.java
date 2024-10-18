f411c6e2d4249f8836a1742a303dfb0714a11a7e
andrey shalagin <andreyshalagin@gmail.com>
2024-10-18 07:25:58 +0300

    Задание 2: 3 аргумента: два параметра-типа, один зафиксированный

--- src/test/java/chapters/fifteen/generics/methods/GenericMethods.java
+++ src/test/java/chapters/fifteen/generics/methods/GenericMethods.java
@@ -1,14 +1,14 @@
 package chapters.fifteen.generics.methods;
 
 public class GenericMethods {
-    public <T, U, V> void func(T x, U y, V z) {
+    public <T, U>  void func(T x, U y, String s) {
         System.out.println(x.getClass().getName());
         System.out.println(y.getClass().getName());
-        System.out.println(z.getClass().getName());
+        System.out.println(s.getClass().getName());
     }
 
     public static void main(String[] args) {
         GenericMethods gm = new GenericMethods();
-        gm.func("Hi", 10, 10.1);
+        gm.func(1, 1.7, "Hi!");
     }
 }
