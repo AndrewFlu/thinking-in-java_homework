10f179f1fea927bb6be1baa5039776c0f9837ccf
andrey shalagin <andreyshalagin@gmail.com>
2024-10-18 07:23:37 +0300

    Задание 1: В метод func добавлена поддержка 3-х параметров-типов

--- src/test/java/chapters/fifteen/generics/methods/GenericMethods.java
+++ src/test/java/chapters/fifteen/generics/methods/GenericMethods.java
@@ -1,16 +1,14 @@
 package chapters.fifteen.generics.methods;
 
 public class GenericMethods {
-    public <T> void func(T x) {
+    public <T, U, V> void func(T x, U y, V z) {
         System.out.println(x.getClass().getName());
+        System.out.println(y.getClass().getName());
+        System.out.println(z.getClass().getName());
     }
 
     public static void main(String[] args) {
         GenericMethods gm = new GenericMethods();
-        gm.func("Hi");
-        gm.func(10);
-        gm.func(10.5);
-        gm.func(10.7f);
-
+        gm.func("Hi", 10, 10.1);
     }
 }
