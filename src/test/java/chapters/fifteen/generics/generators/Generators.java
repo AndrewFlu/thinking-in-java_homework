13507358dcd05fdd681be523060821b124ba3af9
andrey shalagin <andreyshalagin@gmail.com>
2024-10-19 09:22:09 +0300

    Задание 13: перегруженные версии fill() для конкретных реализаций Collection

--- src/test/java/chapters/fifteen/generics/generators/Generators.java
+++ src/test/java/chapters/fifteen/generics/generators/Generators.java
@@ -5,20 +5,44 @@ import chapters.fifteen.generics.cofee.CoffeeGenerator;
 import chapters.fifteen.generics.fibonacci.FibonacciGenerator;
 import utils.Generator;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 public class Generators {
     public static <T> Collection<T> fill(Collection<T> collection, Generator<T> gen, int n) {
+        System.out.println("version for Collection");
         for (int i = 0; i < n; i++) {
             collection.add(gen.next());
         }
-
         return collection;
+    }
+
+    public static <T> List<T> fill(List<T> list, Generator<T> gen, int n) {
+        System.out.println("version for List");
+        for (int i = 0; i < n; i++) {
+            list.add(gen.next());
+        }
+        return list;
+    }
+
+    public static <T> Set<T> fill(Set<T> set, Generator<T> gen, int n) {
+        System.out.println("version for Set");
+        for (int i = 0; i < n; i++) {
+            set.add(gen.next());
+        }
+        return set;
+    }
+
+    public static <T> Queue<T> fill(Queue<T> queue, Generator<T> gen, int n) {
+        System.out.println("version for Queue");
+        for (int i = 0; i < n; i++) {
+            queue.add(gen.next());
+        }
+        return queue;
     }
 
     // test-drive
     public static void main(String[] args) {
+        // List
         Collection<Coffee> coffees = fill(
                 new ArrayList<Coffee>(),
                 new CoffeeGenerator(),
@@ -29,14 +53,27 @@ public class Generators {
             System.out.println(c);
         }
 
-        Collection<Integer> fnumbers = fill(
-                new ArrayList<>(),
-                new FibonacciGenerator(),
-                10
-        );
+        // Set
+        Generator<String> stringGenerator = new StringGenerator();
+        Collection<String> stringSet = fill(new HashSet<>(), stringGenerator, 3);
+        for (String s : stringSet) {
+            System.out.println(s);
+        }
 
-        for (int i : fnumbers) {
-            System.out.println(i);
+        //Queue
+        Collection<Integer> fibonacci = fill(new PriorityQueue<>(), new FibonacciGenerator(), 10);
+        for (Integer f : fibonacci) {
+            System.out.println(f);
+        }
+    }
+
+    private static class StringGenerator implements Generator<String> {
+        private Random random = new Random();
+
+        @Override
+        public String next() {
+            char[] chars = {'a', 'b', 'c'};
+            return String.valueOf(chars[random.nextInt(chars.length)]);
         }
     }
 }
