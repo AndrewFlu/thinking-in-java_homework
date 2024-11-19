c2cf0b77e9bad923fcf7b6c684ffd388f9c4429f
andrey shalagin <andreyshalagin@gmail.com>
2024-11-19 07:32:59 +0300

    Приведение типа при использовании обобщений: стандартное приведение

---
+++ src/test/java/chapters/fifteen/generics/casting/NeedCasting.java
@@ -0,0 +1,19 @@
+package chapters.fifteen.generics.casting;
+
+import chapters.fifteen.generics.instantiate.withconstraint.Widget;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.List;
+
+public class NeedCasting {
+    @SuppressWarnings("unchecked")
+    public void f(String[] args) throws IOException, ClassNotFoundException {
+        ObjectInputStream in = new ObjectInputStream(
+                new FileInputStream(args[0])
+        );
+
+        List<Widget> shapes = (List<Widget>) in.readObject();
+    }
+}
