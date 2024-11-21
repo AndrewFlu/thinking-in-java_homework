76bb66c13dbc134d18c4eec85f15fc7eb601cef4
andrey shalagin <andreyshalagin@gmail.com>
2024-11-21 09:49:22 +0300

    Перехват интерфейса базовым классом: при наследовании можно либо повторно реализовать интерфейс в ComparablePet при условии его точного совпадения. Либо просто переопределить метод базового класса

---
+++ src/test/java/chapters/fifteen/generics/comparable/Gecko.java
@@ -0,0 +1,7 @@
+package chapters.fifteen.generics.comparable;
+
+public class Gecko extends ComparablePet {
+    public int compareTo(ComparablePet arg) {
+        return 0;
+    }
+}
---
+++ src/test/java/chapters/fifteen/generics/comparable/Hamster.java
@@ -0,0 +1,8 @@
+package chapters.fifteen.generics.comparable;
+
+public class Hamster extends ComparablePet implements Comparable<ComparablePet>{
+    public int compareTo(ComparablePet arg) {
+        return 0;
+    }
+
+}
