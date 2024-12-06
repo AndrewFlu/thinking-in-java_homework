a6216e38a04820ce0a1c611e98ae572e110310e1
andrey shalagin <andreyshalagin@gmail.com>
2024-12-06 08:23:34 +0300

    Задание 39: Добавлен ещё один класс примеси Colored в DynamicProxyMixinTestDrive

--- src/test/java/chapters/fifteen/generics/mixins/dynamicproxy/DynamicProxyMixinTestDrive.java
+++ src/test/java/chapters/fifteen/generics/mixins/dynamicproxy/DynamicProxyMixinTestDrive.java
@@ -2,6 +2,8 @@ package chapters.fifteen.generics.mixins.dynamicproxy;
 
 import chapters.fifteen.generics.mixins.*;
 
+import java.awt.*;
+
 import static chapters.fifteen.generics.tuples.inferedtypes.Tuple.tuple;
 
 public class DynamicProxyMixinTestDrive {
@@ -9,16 +11,20 @@ public class DynamicProxyMixinTestDrive {
         Object mixin = MixinProxy.newInstance(
                 tuple(new BasicImpl(), Basic.class),
                 tuple(new TimeStampedImpl(), TimeStamped.class),
-                tuple(new SerialNumberedImpl(), SerialNumbered.class)
+                tuple(new SerialNumberedImpl(), SerialNumbered.class),
+                tuple(new ColoredImpl(), Colored.class)
         );
 
         Basic b = (Basic) mixin;
         TimeStamped t = (TimeStamped) mixin;
         SerialNumbered s = (SerialNumbered) mixin;
+        Colored c = (Colored) mixin;
 
         b.set("Hi!");
+        c.setColor(Color.BLUE);
         System.out.println(b.get());
         System.out.println(t.getStamp());
         System.out.println(s.getSerialNumbered());
+        System.out.println(c.getColor());
     }
 }
