610388dc24360453aaa952a0a8f703bcd0500428
andrey shalagin <andreyshalagin@gmail.com>
2024-12-21 11:41:45 +0300

    Задание 10: ArrayOfGeneric переведён на контейнеры

--- src/test/java/chapters/sixteen/arrays/generics/ArrayOfGenerics.java
+++ src/test/java/chapters/sixteen/arrays/generics/ArrayOfGenerics.java
@@ -3,31 +3,32 @@ package chapters.sixteen.arrays.generics;
 import chapters.sixteen.arrays.containercomparison.BerylliumSphere;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ArrayOfGenerics {
-    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        List<String>[] listOfStrings;
-        List[] listArray = new List[10];
+        List<List<String>> listOfStrings = new ArrayList<>();
+//        List[] listArray = new List[10];
 
-        listOfStrings = (List<String>[])listArray; // неконтролируемое предупреждение
-        listOfStrings[0] = new ArrayList<String>();
+//        listOfStrings = listArray; // неконтролируемое предупреждение
+        listOfStrings.add(new ArrayList<String>());
+
+        System.out.println(listOfStrings);
+        System.out.println();
 //        listOfStrings[1] = new ArrayList<Integer>(); // ошибка времени компиляции
 
         // проблема: List<String> является подтипом Object
-        Object[] objects = listOfStrings; // значит, присваивание допустимо
+//        Object[] objects = listOfStrings; // значит, присваивание допустимо
         // Компилируется и выполняется без ошибок
-        objects[1] = new ArrayList<Integer>();
+//        objects[1] = new ArrayList<Integer>();
 
         // Однако при простейших потребностях можно создать массив обобщений,
         // хотя и с неконтролируемым предупреждением:
-        List<BerylliumSphere>[] spheres = (List<BerylliumSphere>[])new List[10];
-        for (int i = 0; i < spheres.length; i++) {
-            spheres[i] = new ArrayList<BerylliumSphere>();
-        }
+        List<List<BerylliumSphere>> spheres = new ArrayList<>(10);
+        spheres.add(new ArrayList<>(2));
+        spheres.add(new ArrayList<>(3));
+        spheres.add(new ArrayList<>(List.of(new BerylliumSphere(), new BerylliumSphere())));
 
-        System.out.println(Arrays.toString(spheres));
+        System.out.println(spheres);
     }
 }
