fdb41961e445e9a2de5530fccbf206732c18d183
andrey shalagin <andreyshalagin@gmail.com>
2024-12-21 09:57:40 +0300

    Задание 7: создание трёхмерного массива BerylliumSphere

--- src/test/java/chapters/sixteen/arrays/multydimensionalarrays/BerylliumTestDrive.java
+++ src/test/java/chapters/sixteen/arrays/multydimensionalarrays/BerylliumTestDrive.java
@@ -6,17 +6,34 @@ import java.util.Arrays;
 
 public class BerylliumTestDrive {
     public static void main(String[] args) {
-        BerylliumSphere[][] spheres = getBerylliumSpheresArray(3, 4);
+        BerylliumSphere[][] doubleArraySpheres = getDoubleBerylliumSpheresArray(3, 4);
+        System.out.println(Arrays.deepToString(doubleArraySpheres));
 
-        System.out.println(Arrays.deepToString(spheres));
+        BerylliumSphere[][][] tripleBerylliumSpheresArray = getTripleBerylliumSpheresArray(2, 5, 1);
+        System.out.println(Arrays.deepToString(tripleBerylliumSpheresArray));
     }
 
-    private static BerylliumSphere[][] getBerylliumSpheresArray(int firstArrLength, int secArrLength) {
+    private static BerylliumSphere[][] getDoubleBerylliumSpheresArray(int firstArrLength, int secArrLength) {
         BerylliumSphere[][] result = new BerylliumSphere[firstArrLength][];
         for (int i = 0; i < result.length; i++) {
             result[i] = new BerylliumSphere[secArrLength];
             for (int j = 0; j < result[i].length; j++) {
                 result[i][j] = new BerylliumSphere();
+            }
+        }
+
+        return result;
+    }
+
+    private static BerylliumSphere[][][] getTripleBerylliumSpheresArray(int firstArrLength, int secArrLength, int thirdArrLength) {
+        BerylliumSphere[][][] result = new BerylliumSphere[firstArrLength][][];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new BerylliumSphere[secArrLength][];
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = new BerylliumSphere[thirdArrLength];
+                for (int k = 0; k < result[i][j].length; k++) {
+                    result[i][j][k] = new BerylliumSphere();
+                }
             }
         }
 
