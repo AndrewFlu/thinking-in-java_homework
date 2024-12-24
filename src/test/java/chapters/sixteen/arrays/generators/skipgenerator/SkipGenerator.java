e96f940cbe43d57d89960a6a8cb6713321ce2220
andrey shalagin <andreyshalagin@gmail.com>
2024-12-24 09:13:20 +0300

    Задание 16: Создание массивов с новым генератором SkipGenerator, котрый создаёт массивы целевого типа с приращением, передаваемым в конструкторе

---
+++ src/test/java/chapters/sixteen/arrays/generators/skipgenerator/SkipGenerator.java
@@ -0,0 +1,143 @@
+package chapters.sixteen.arrays.generators.skipgenerator;
+
+import utils.Generator;
+
+public class SkipGenerator {
+
+    public static class Boolean implements Generator<java.lang.Boolean> {
+        private boolean value;
+//        private boolean increment;
+
+        public Boolean(boolean increment) {
+            value = increment;
+        }
+
+        @Override
+        public java.lang.Boolean next() {
+            boolean oldValue = value;
+            value = !value;
+
+            return oldValue;
+        }
+    }
+
+    public static class Byte implements Generator<java.lang.Byte> {
+        private byte value;
+        private byte increment;
+
+        public Byte(byte increment) {
+            this.increment = increment;
+        }
+
+        @Override
+        public java.lang.Byte next() {
+            byte oldValue = value;
+            value += increment;
+
+            return oldValue;
+        }
+    }
+    static char[] chars = ("abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ").toCharArray();
+    public static class Character implements Generator<java.lang.Character> {
+
+        private int index = 0;
+        private int increment;
+
+        public Character(int increment) {
+            this.increment = increment;
+        }
+
+        @Override
+        public java.lang.Character next() {
+            int oldIndex = index % chars.length;
+            index  += increment;
+
+            return chars[oldIndex];
+        }
+    }
+
+    public static class Short implements Generator<java.lang.Short> {
+        private short value;
+        private short increment;
+
+        public Short(short increment) {
+            this.increment = increment;
+        }
+
+        @Override
+        public java.lang.Short next() {
+            short oldValue = value;
+            value += increment;
+
+            return oldValue;
+        }
+    }
+
+    public static class Integer implements Generator<java.lang.Integer> {
+        private int value;
+        private int increment;
+
+        public Integer(int increment) {
+            this.increment = increment;
+        }
+
+        @Override
+        public java.lang.Integer next() {
+            int oldValue = value;
+            value += increment;
+
+            return oldValue;
+        }
+    }
+
+    public static class Long implements Generator<java.lang.Long> {
+        private long value;
+        private long increment;
+
+        public Long(long increment) {
+            this.increment = increment;
+        }
+
+        @Override
+        public java.lang.Long next() {
+            long oldValue = value;
+            value += increment;
+
+            return oldValue;
+        }
+    }
+
+    public static class Float implements Generator<java.lang.Float> {
+        private float value;
+        private float increment;
+
+        public Float(float increment) {
+            this.increment = increment;
+        }
+
+        @Override
+        public java.lang.Float next() {
+            float oldValue = value;
+            value += increment;
+
+            return oldValue;
+        }
+    }
+
+    public static class Double implements Generator<java.lang.Double> {
+        private double value;
+        private double increment;
+
+        public Double(double increment) {
+            this.increment = increment;
+        }
+
+        @Override
+        public java.lang.Double next() {
+            double oldValue = value;
+            value += increment;
+
+            return oldValue;
+        }
+    }
+}
---
+++ src/test/java/chapters/sixteen/arrays/generators/skipgenerator/SkipGeneratorTestDrive.java
@@ -0,0 +1,40 @@
+package chapters.sixteen.arrays.generators.skipgenerator;
+
+import chapters.sixteen.arrays.generators.Generated;
+import chapters.sixteen.arrays.generators.converttoprimitives.ConvertTo;
+
+import java.util.Arrays;
+
+import static net.midview.Print.print;
+
+public class SkipGeneratorTestDrive {
+    public static void main(String[] args) {
+        int size = 5;
+
+        boolean[] a1 = ConvertTo.primitive(Generated.array(Boolean.class, new SkipGenerator.Boolean(true), size));
+        print("boolean[] a1 = " + Arrays.toString(a1));
+
+        byte[] a2 = ConvertTo.primitive(Generated.array(Byte.class, new SkipGenerator.Byte((byte) 3), size));
+        print("byte[] a2 = " + Arrays.toString(a2));
+
+        char[] a3 = ConvertTo.primitive(Generated.array(Character.class, new SkipGenerator.Character(2), size));
+        print("char[] a3 = " + Arrays.toString(a3));
+
+        short[] a4 = ConvertTo.primitive(Generated.array(Short.class, new SkipGenerator.Short((short) 2), size));
+        print("short[] a4 = " + Arrays.toString(a4));
+
+        int[] a5 = ConvertTo.primitive(Generated.array(Integer.class, new SkipGenerator.Integer(2), size));
+        print("int[] a5 = " + Arrays.toString(a5));
+
+        long[] a6 = ConvertTo.primitive(Generated.array(Long.class, new SkipGenerator.Long(3), size));
+        print("long[] a6 = " + Arrays.toString(a6));
+
+        float[] a7 = ConvertTo.primitive(Generated.array(Float.class, new SkipGenerator.Float(3), size));
+        print("float[] a7 = " + Arrays.toString(a7));
+
+        double[] a8 = ConvertTo.primitive(Generated.array(Double.class, new SkipGenerator.Double(3), size));
+        print("double[] a8 = " + Arrays.toString(a8));
+
+
+    }
+}
