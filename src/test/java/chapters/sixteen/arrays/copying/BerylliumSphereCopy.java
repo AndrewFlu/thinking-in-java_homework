931c882ad8d1c9024a2b879ac58540da9602750c
andrey shalagin <andreyshalagin@gmail.com>
2024-12-28 08:17:44 +0300

    Задание 18: доказательство, что массивы объектов копируются "поверхностным" копированием

---
+++ src/test/java/chapters/sixteen/arrays/copying/BerylliumSphereCopy.java
@@ -0,0 +1,23 @@
+package chapters.sixteen.arrays.copying;
+
+import chapters.sixteen.arrays.containercomparison.BerylliumSphere;
+import chapters.sixteen.arrays.generators.BerylliumSphereGenerator;
+import chapters.sixteen.arrays.generators.Generated;
+
+import java.util.Arrays;
+
+import static net.midview.Print.print;
+
+public class BerylliumSphereCopy {
+    public static void main(String[] args) {
+        BerylliumSphere[] srcArray = new BerylliumSphere[5];
+        Generated.array(srcArray, new BerylliumSphereGenerator());
+        print("srcArray = " + Arrays.toString(srcArray));
+
+        BerylliumSphere[] targetArray = new BerylliumSphere[10];
+        System.arraycopy(srcArray, 0, targetArray, 0, srcArray.length);
+        print("targetArray = " + Arrays.toString(targetArray));
+        print("srcArray = " + Arrays.toString(srcArray));
+        print("is first element the same? " + srcArray[0].equals(targetArray[0]));
+    }
+}
