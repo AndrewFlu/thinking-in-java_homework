63519a0b2d88cddd21beacaf2736c96c95ef0eeb
andrey shalagin <andreyshalagin@gmail.com>
2024-12-29 08:33:48 +0300

    Сравнение массивов по второму подходу: через указание кастомного компаратора (Collections.reverseOrder())

---
+++ src/test/java/chapters/sixteen/arrays/comparing/Reverse.java
@@ -0,0 +1,23 @@
+package chapters.sixteen.arrays.comparing;
+
+import chapters.sixteen.arrays.generators.Generated;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static net.midview.Print.print;
+
+public class Reverse {
+    public static void main(String[] args) {
+        CompType[] compTypesArray = Generated.array(new CompType[10], CompType.generator());
+        print("before sorting:");
+        print(Arrays.toString(compTypesArray));
+        Arrays.sort(compTypesArray);
+        print("after sorting:");
+        print(Arrays.toString(compTypesArray));
+        print("");
+        Arrays.sort(compTypesArray, Collections.reverseOrder());
+        print("after sorting with custom comparator (Collections.reverseOrder()):");
+        print(Arrays.toString(compTypesArray));
+    }
+}
