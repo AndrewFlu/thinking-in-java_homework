0ee7df14eea79e9c5def774cbe54b71a55482685
andrey shalagin <andreyshalagin@gmail.com>
2024-12-30 12:41:18 +0300

    Поиск в отсортированном массиве (Arrays.binarySearch())

---
+++ src/test/java/chapters/sixteen/arrays/search/ArraySearching.java
@@ -0,0 +1,28 @@
+package chapters.sixteen.arrays.search;
+
+import chapters.sixteen.arrays.generators.Generated;
+import chapters.sixteen.arrays.generators.RandomGenerator;
+import chapters.sixteen.arrays.generators.converttoprimitives.ConvertTo;
+import utils.Generator;
+
+import java.util.Arrays;
+
+import static net.midview.Print.print;
+
+public class ArraySearching {
+    public static void main(String[] args) {
+        Generator<Integer> generator = new RandomGenerator.Integer(1000);
+        int[] intArray = ConvertTo.primitive(Generated.array(new Integer[25], generator));
+        Arrays.sort(intArray);
+        print("sorted array: " + Arrays.toString(intArray));
+
+        while (true) {
+            int randomNumberToSearch = generator.next();
+            int resultIndex = Arrays.binarySearch(intArray, randomNumberToSearch);
+            if (resultIndex >= 0) {
+                System.out.printf("Location index of %s is %s, intArray[%s]", resultIndex, intArray[resultIndex], resultIndex);
+                break;
+            }
+        }
+    }
+}
