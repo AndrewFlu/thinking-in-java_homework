21f119185c7cfb98155d350c91646b6810196dc0
andrey shalagin <andreyshalagin@gmail.com>
2025-01-03 10:29:20 +0300

    Пример использования генератора для заполения коллекций (тестовых данных)

---
+++ src/test/java/chapters/containers/generators/CollectionData.java
@@ -0,0 +1,24 @@
+package chapters.containers.generators;
+
+import chapters.sixteen.arrays.generators.RandomGenerator;
+import utils.Generator;
+
+import java.util.ArrayList;
+
+public class CollectionData<T> extends ArrayList<T> {
+    public CollectionData(Generator<T> gen, int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            add(gen.next());
+        }
+    }
+
+    // обобщённый метод
+    public static <T> CollectionData<T> list(Generator<T> gen, int quantity) {
+        return new CollectionData<T>(gen, quantity);
+    }
+
+    public static void main(String[] args) {
+        CollectionData<Integer> intCollection = CollectionData.list(new RandomGenerator.Integer(), 5);
+        System.out.println(intCollection);
+    }
+}
