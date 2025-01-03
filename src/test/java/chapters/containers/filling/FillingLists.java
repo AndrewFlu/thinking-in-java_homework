5ed11147d6a725faf621ce9231eb540484bd13fc
andrey shalagin <andreyshalagin@gmail.com>
2025-01-03 10:06:00 +0300

    Пример заполнения коллекций одной ссылкой на один и тот же объект: * Collections.fill(); * Collections.nCopies();

---
+++ src/test/java/chapters/containers/filling/FillingLists.java
@@ -0,0 +1,15 @@
+package chapters.containers.filling;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FillingLists {
+    public static void main(String[] args) {
+        List<StringAddress> list = new ArrayList<StringAddress>(Collections.nCopies(5, new StringAddress("Hello")));
+        System.out.println(list);
+
+        Collections.fill(list, new StringAddress("World!"));
+        System.out.println(list);
+    }
+}
