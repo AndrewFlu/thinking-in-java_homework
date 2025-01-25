c8c1af97736ad92a30a1e5269cbd431dc9536142
andrey shalagin <andreyshalagin@gmail.com>
2025-01-25 09:04:48 +0300

    Пример реализации программы "Список дел" с поддержкой приоритизации (через внутренний класс с имплементацией Comparable) на основе расширения функциональности PriorityQueue, типизированной внутренним классом

---
+++ src/test/java/chapters/eleven/containers/set/queue/ToDoList.java
@@ -0,0 +1,51 @@
+package chapters.eleven.containers.set.queue;
+
+import java.util.PriorityQueue;
+
+public class ToDoList extends PriorityQueue<ToDoList.ToDoItem> {
+
+    static class ToDoItem implements Comparable<ToDoItem> {
+        private char primary;
+        private int secondary;
+        private String item;
+
+        public ToDoItem(char primary, int secondary, String item) {
+            this.primary = primary;
+            this.secondary = secondary;
+            this.item = item;
+        }
+
+        @Override
+        public int compareTo(ToDoItem arg) {
+            if (primary > arg.primary)
+                return +1;
+            if (primary == arg.primary)
+                if (secondary > arg.secondary)
+                    return +1;
+                else if (secondary == arg.secondary)
+                    return 0;
+            return -1;
+        }
+
+        @Override
+        public String toString() {
+            return Character.toString(primary) + secondary + ": " + item;
+        }
+    }
+
+    public void add(String toDo, char priority, int secondary) {
+        super.add(new ToDoItem(priority, secondary, toDo));
+    }
+
+    // test-drive
+    public static void main(String[] args) {
+        ToDoList toDoList = new ToDoList();
+        toDoList.add("Read 'Web security'", 'A', 3);
+        toDoList.add("Read 'Thinking in Java'", 'A', 2);
+        toDoList.add("Cover the flour", 'C', 1);
+
+        while (!toDoList.isEmpty()) {
+            System.out.println(toDoList.remove());
+        }
+    }
+}
