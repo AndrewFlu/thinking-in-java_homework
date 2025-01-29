cd4f3cef514ffdf35fc8c33fe64973b905256a14
andrey shalagin <andreyshalagin@gmail.com>
2025-01-29 08:12:59 +0300

    Операции с Map

---
+++ src/test/java/chapters/eleven/containers/map/MapOperations.java
@@ -0,0 +1,50 @@
+package chapters.eleven.containers.map;
+
+import chapters.containers.lightweight.CountingMapData;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static net.midview.Print.print;
+
+public class MapOperations {
+    public static void printKeys(Map<Integer, String> map) {
+        System.out.print("Size: " + map.size() + ", ");
+        System.out.print("Keys: ");
+        System.out.println(map.keySet());
+    }
+
+    public static void test(Map<Integer, String> map) {
+        print(map.getClass().getSimpleName());
+        map.putAll(new CountingMapData(25));
+        // Map реализует поведение 'Set' для ключей
+        printKeys(map);
+        // Создание коллекции значений
+        System.out.print("Values: ");
+        System.out.println(map.values());
+        System.out.println(map);
+        System.out.println("map.containsKey(11): " + map.containsKey(11));
+        System.out.println("map.get(11): " + map.get(11));
+        System.out.println("map.containsValue('F0'): " + map.containsValue("F0"));
+        Integer key = map.keySet().iterator().next();
+        System.out.println("First key in map: " + key);
+        map.remove(key);
+        printKeys(map);
+        map.clear();
+        print("map.isEmpty(): " + map.isEmpty());
+        map.putAll(new CountingMapData(25));
+        print("map.isEmpty(): " + map.isEmpty());
+        // Операции с Set изменяют Map
+        map.keySet().removeAll(map.keySet());
+        print("map.isEmpty(): " + map.isEmpty());
+    }
+
+    public static void main(String[] args) {
+        test(new HashMap<Integer, String>());
+        test(new TreeMap<Integer, String>());
+        test(new LinkedHashMap<>());
+        test(new IdentityHashMap<>());
+        test(new ConcurrentHashMap<>());
+        test(new WeakHashMap<>());
+    }
+}
