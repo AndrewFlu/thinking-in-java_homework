94715e443665731b915e59a95f51e7cd5fff609e
andrey shalagin <andreyshalagin@gmail.com>
2025-02-12 08:06:07 +0300

    Одинаковые строки имеют одинаковое значение хэш-кода

---
+++ src/test/java/chapters/eleven/containers/hashcodes/StringHashCode.java
@@ -0,0 +1,9 @@
+package chapters.eleven.containers.hashcodes;
+
+public class StringHashCode {
+    public static void main(String[] args) {
+        String[] hellos = "Hello Hello".split(" ");
+        System.out.println(hellos[0].hashCode());
+        System.out.println(hellos[1].hashCode());
+    }
+}
