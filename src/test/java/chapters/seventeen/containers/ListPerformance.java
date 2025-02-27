93580ccb67da2c5b940701e7020e090f3124a56d
andrey shalagin <andreyshalagin@gmail.com>
2025-02-27 07:19:09 +0300

    Задание 29: Заменён тип объектов в контейнерах с Integer на String

--- src/test/java/chapters/seventeen/containers/ListPerformance.java
+++ src/test/java/chapters/seventeen/containers/ListPerformance.java
@@ -5,6 +5,8 @@ package chapters.seventeen.containers;
 // {Args: 100 500} Небольшие значения,
 // чтобы тесты не занимали много времени
 
+import chapters.containers.countries.Countries;
+import chapters.containers.generators.CollectionData;
 import chapters.containers.lightweight.CountingIntegerList;
 import chapters.sixteen.arrays.generators.CountingGenerator;
 import chapters.sixteen.arrays.generators.Generated;
@@ -14,21 +16,21 @@ import java.util.*;
 public class ListPerformance {
     static Random rand = new Random();
     static int reps = 1000;
-    static List<Test<List<Integer>>> tests = new ArrayList<Test<List<Integer>>>();
-    static List<Test<LinkedList<Integer>>> qTests = new ArrayList<Test<LinkedList<Integer>>>();
+    static List<Test<List<String>>> tests = new ArrayList<>();
+    static List<Test<LinkedList<String>>> qTests = new ArrayList<>();
 
     static {
         // 1. Тестирование поведения списка (List)
         // add
-        tests.add(new Test<List<Integer>>("add") {
+        tests.add(new Test<List<String>>("add") {
             @Override
-            int test(List<Integer> container, TestParam tp) {
+            int test(List<String> container, TestParam tp) {
                 int loops = tp.loops;
                 int containerSize = tp.size;
                 for (int i = 0; i < loops; i++) {
                     container.clear();
                     for (int j = 0; j < containerSize; j++)
-                        container.add(j);
+                        container.add(String.valueOf(j));
                 }
 
                 return loops * containerSize;
@@ -36,9 +38,9 @@ public class ListPerformance {
         });
 
         // get
-        tests.add(new Test<List<Integer>>("get") {
+        tests.add(new Test<List<String>>("get") {
             @Override
-            int test(List<Integer> container, TestParam tp) {
+            int test(List<String> container, TestParam tp) {
                 int loops = tp.loops * reps;
                 int containerSize = container.size();
                 for (int i = 0; i < loops; i++)
@@ -49,54 +51,60 @@ public class ListPerformance {
         });
 
         // set
-        tests.add(new Test<List<Integer>>("set") {
+        tests.add(new Test<List<String>>("set") {
             @Override
-            int test(List<Integer> container, TestParam tp) {
+            int test(List<String> container, TestParam tp) {
                 int loops = tp.loops * reps;
                 int containerSize = container.size();
                 for (int i = 0; i < loops; i++)
-                    container.set(rand.nextInt(containerSize), 47);
+                    container.set(rand.nextInt(containerSize), String.valueOf(47));
 
                 return loops;
             }
         });
 
         // iter add
-        tests.add(new Test<List<Integer>>("iteradd") {
+        tests.add(new Test<List<String>>("iteradd") {
             @Override
-            int test(List<Integer> container, TestParam tp) {
+            int test(List<String> container, TestParam tp) {
                 final int LOOPS = 1_000_000;
                 int half = container.size() / 2;
-                ListIterator<Integer> it = container.listIterator(half);
+                ListIterator<String> it = container.listIterator(half);
                 for (int i = 0; i < LOOPS; i++)
-                    it.add(47);
+                    it.add(String.valueOf(47));
 
                 return LOOPS;
             }
         });
 
         // insert
-        tests.add(new Test<List<Integer>>("insert") {
+        tests.add(new Test<List<String>>("insert") {
             @Override
-            int test(List<Integer> container, TestParam tp) {
+            int test(List<String> container, TestParam tp) {
                 int loops = tp.loops;
                 for (int i = 0; i < loops; i++)
-                    container.add(5, 47); // Минимизация затрат на произвольный доступ
+                    container.add(5, String.valueOf(47)); // Минимизация затрат на произвольный доступ
 
                 return loops;
             }
         });
 
+//        tests.add(new Test<List<String>>("sort") {
+//            @Override
+//            int test(List<String> container, TestParam tp) {
+//
+//            }
+//        });
 
         // remove
-        tests.add(new Test<List<Integer>>("remove") {
+        tests.add(new Test<List<String>>("remove") {
             @Override
-            int test(List<Integer> container, TestParam tp) {
+            int test(List<String> container, TestParam tp) {
                 int loops = tp.loops;
-                int size = container.size();
+                int size = tp.size;
                 for (int i = 0; i < loops; i++) {
                     container.clear();
-                    container.addAll(new CountingIntegerList(size));
+                    container.addAll(CollectionData.list(new CountingGenerator.String(), size));
                     while (container.size() > 5)
                         container.remove(5); // Минимизация затрат на произвольный доступ
                 }
@@ -107,15 +115,15 @@ public class ListPerformance {
 
         // 2. Тестирование поведения очереди (LinkedList)
         // add first
-        qTests.add(new Test<LinkedList<Integer>>("addFirst") {
+        qTests.add(new Test<LinkedList<String>>("addFirst") {
             @Override
-            int test(LinkedList<Integer> container, TestParam tp) {
+            int test(LinkedList<String> container, TestParam tp) {
                 int loops = tp.loops;
                 int size = tp.size;
                 for (int i = 0; i < loops; i++) {
                     container.clear();
                     for (int j = 0; j < size; j++)
-                        container.addFirst(47);
+                        container.addFirst(String.valueOf(47));
                 }
 
                 return loops * size;
@@ -123,15 +131,15 @@ public class ListPerformance {
         });
 
         // add last
-        qTests.add(new Test<LinkedList<Integer>>("addLast") {
+        qTests.add(new Test<LinkedList<String>>("addLast") {
             @Override
-            int test(LinkedList<Integer> container, TestParam tp) {
+            int test(LinkedList<String> container, TestParam tp) {
                 int loops = tp.loops;
                 int size = tp.size;
                 for (int i = 0; i < loops; i++) {
                     container.clear();
                     for (int j = 0; j < size; j++)
-                        container.addLast(47);
+                        container.addLast(String.valueOf(47));
                 }
 
                 return loops * size;
@@ -139,14 +147,14 @@ public class ListPerformance {
         });
 
         // rm first
-        qTests.add(new Test<LinkedList<Integer>>("rmFirst") {
+        qTests.add(new Test<LinkedList<String>>("rmFirst") {
             @Override
-            int test(LinkedList<Integer> container, TestParam tp) {
+            int test(LinkedList<String> container, TestParam tp) {
                 int loops = tp.loops;
                 int size = tp.size;
                 for (int i = 0; i < loops; i++) {
                     container.clear();
-                    container.addAll(new CountingIntegerList(size));
+                    container.addAll(CollectionData.list(new CountingGenerator.String(), size));
                     while (container.size() > 0)
                         container.removeFirst();
                 }
@@ -155,14 +163,14 @@ public class ListPerformance {
         });
 
         // rm last
-        qTests.add(new Test<LinkedList<Integer>>("rmLast") {
+        qTests.add(new Test<LinkedList<String>>("rmLast") {
             @Override
-            int test(LinkedList<Integer> container, TestParam tp) {
+            int test(LinkedList<String> container, TestParam tp) {
                 int loops = tp.loops;
                 int size = tp.size;
                 for (int i = 0; i < loops; i++) {
                     container.clear();
-                    container.addAll(new CountingIntegerList(size));
+                    container.addAll(CollectionData.list(new CountingGenerator.String(), size));
                     while (container.size() > 0)
                         container.removeLast();
                 }
@@ -172,22 +180,22 @@ public class ListPerformance {
         });
     }
 
-    static class ListTester extends Tester<List<Integer>> {
-        public ListTester(List<Integer> container, List<Test<List<Integer>>> tests) {
+    static class ListTester extends Tester<List<String>> {
+        public ListTester(List<String> container, List<Test<List<String>>> tests) {
             super(container, tests);
         }
 
         // Заполняется до нужного размера перед каждым тестом
         @Override
-        protected List<Integer> initialise(int size) {
+        protected List<String> initialise(int size) {
             container.clear();
-            container.addAll(new CountingIntegerList(size));
+            container.addAll(CollectionData.list(new CountingGenerator.String(), size));
 
             return container;
         }
 
         // Вспомогательный метод
-        public static void run(List<Integer> list, List<Test<List<Integer>>> tests) {
+        public static void run(List<String> list, List<Test<List<String>>> tests) {
             new ListTester(list, tests).timedTest();
         }
 
@@ -195,15 +203,15 @@ public class ListPerformance {
             if (args.length > 0)
                 Tester.defaultParams = TestParam.array(args);
             // Выполняет с массивом только эти два теста
-            Tester<List<Integer>> arrayTest = new Tester<List<Integer>>(null, tests.subList(1, 3)) {
+            Tester<List<String>> arrayTest = new Tester<List<String>>(null, tests.subList(1, 3)) {
 
                 // Будет вызываться перед каждым тестом. Создаёт список
                 // на базе массивабез возмоджности изменения размера:
                 @Override
-                protected List<Integer> initialise(int size) {
-                    Integer[] ia = Generated.array(Integer.class, new CountingGenerator.Integer(), size);
+                protected List<String> initialise(int size) {
+                    String[] sa = Generated.array(String.class, new CountingGenerator.String(5), size);
 
-                    return Arrays.asList(ia);
+                    return Arrays.asList(sa);
                 }
             };
 
@@ -213,12 +221,12 @@ public class ListPerformance {
                     10, 5000, 100, 5000, 1000, 1000, 10000, 200);
             if (args.length > 0)
                 Tester.defaultParams = TestParam.array(args);
-            ListTester.run(new ArrayList<Integer>(), tests);
-            ListTester.run(new LinkedList<Integer>(), tests);
-            ListTester.run(new Vector<Integer>(), tests);
+            ListTester.run(new ArrayList<String>(), tests);
+            ListTester.run(new LinkedList<String>(), tests);
+            ListTester.run(new Vector<String>(), tests);
 
             Tester.fieldWidth = 12;
-            Tester<LinkedList<Integer>> qTest = new Tester<LinkedList<Integer>>(new LinkedList<>(), qTests);
+            Tester<LinkedList<String>> qTest = new Tester<LinkedList<String>>(new LinkedList<>(), qTests);
             qTest.setHeadLine("Queue tests");
             qTest.timedTest();
         }
