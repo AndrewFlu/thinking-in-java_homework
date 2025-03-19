0c88491668f72019e5a9afb43ca53859c5149039
andrey shalagin <andreyshalagin@gmail.com>
2025-03-19 10:22:03 +0300

    Задание 8: считывание данных из файла. Название файла принимается из командной строки. Иначе — используется дефолтный файл "example.txt"

---
+++ src/test/java/chapters/eighteen/input_optput/input/IOTask8.java
@@ -0,0 +1,34 @@
+package chapters.eighteen.input_optput.input;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class IOTask8 {
+    public static List<String> read(String fileName) throws IOException {
+        String s;
+        List<String> result = new ArrayList<>();
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        while ((s = bufferedReader.readLine()) != null)
+            result.add(s);
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String fileName;
+        if (args.length != 1)
+            fileName = "example.txt";
+        else fileName = args[0];
+        List<String> readStrings = read(fileName);
+        System.out.println("File data in reversed order...");
+        LinkedList<String> ll = new LinkedList<>(readStrings);
+        while (!ll.isEmpty()) {
+            System.out.println(ll.pollLast());
+        }
+    }
+}
