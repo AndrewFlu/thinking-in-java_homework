3b4520ecc8c9c5f9d60df6083fab3b92330caac0
andrey shalagin <andreyshalagin@gmail.com>
2025-03-26 08:37:18 +0300

    Задание 28: модификация Blips.java с примеромо того, что JVM создаст сама конструктор по умолчанию для класса (если других явно объявленных конструкторов нет)

--- src/test/java/chapters/eighteen/input_optput/serialization/externalizible/BlipCheck.java
+++ src/test/java/chapters/eighteen/input_optput/serialization/externalizible/BlipCheck.java
@@ -5,10 +5,13 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 
-public class Blip2 implements Externalizable {
-    public Blip2() {
-        System.out.println("Blip2 Constructor");
-    }
+public class BlipCheck implements Externalizable {
+
+    // Т.к. других явно объявленных конструкторов класса нет, Java сама создаст конструктор по умолчанию при компиляции.
+    // Поэтому необходимости в таком конструкторе нет
+//    public BlipCheck() {
+//        System.out.println("Blip2 Constructor");
+//    }
 
     @Override
     public void writeExternal(ObjectOutput out) throws IOException {
--- src/test/java/chapters/eighteen/input_optput/serialization/externalizible/Blips.java
+++ src/test/java/chapters/eighteen/input_optput/serialization/externalizible/Blips.java
@@ -2,11 +2,11 @@ package chapters.eighteen.input_optput.serialization.externalizible;
 
 import java.io.*;
 
-public class Blips {
+class Blips {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         System.out.println("Constructing objects");
         Blip1 b1 = new Blip1();
-        Blip2 b2 = new Blip2();
+        BlipCheck b2 = new BlipCheck();
         ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Blips.out"));
         System.out.println("Saving objects:");
         out.writeObject(b1);
@@ -17,6 +17,6 @@ public class Blips {
         System.out.println("Recovering b1:");
         b1 = (Blip1) in.readObject();
         System.out.println("Recovering b2:");
-        b2 = (Blip2) in.readObject();
+        b2 = (BlipCheck) in.readObject();
     }
 }
