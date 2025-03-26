111e86a25f41879fac2d10ddf3da8112f8e53a63
andrey shalagin <andreyshalagin@gmail.com>
2025-03-26 07:57:27 +0300

    Поиск класса. Для корректной работы сериализации и десериализации  JVM требуется класс Class целевого объекта

---
+++ src/test/java/chapters/eighteen/input_optput/serialization/Alien.java
@@ -0,0 +1,6 @@
+package chapters.eighteen.input_optput.serialization;
+
+import java.io.Serializable;
+
+public class Alien implements Serializable {
+}
---
+++ src/test/java/chapters/eighteen/input_optput/serialization/FreezeAlien.java
@@ -0,0 +1,11 @@
+package chapters.eighteen.input_optput.serialization;
+
+import java.io.*;
+
+public class FreezeAlien {
+    public static void main(String[] args) throws IOException {
+        ObjectOutput out = new ObjectOutputStream(new FileOutputStream("X.file"));
+        Alien quellek = new Alien();
+        out.writeObject(quellek);
+    }
+}
