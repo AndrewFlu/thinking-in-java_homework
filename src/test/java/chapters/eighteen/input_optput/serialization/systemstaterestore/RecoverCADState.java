b5ab0eb84395175802321752d9b7d2f83a3ef3c0
andrey shalagin <andreyshalagin@gmail.com>
2025-03-27 10:46:11 +0300

    Программа для восстановления состояния автоматизированной системы (CAD)

---
+++ src/test/java/chapters/eighteen/input_optput/serialization/systemstaterestore/RecoverCADState.java
@@ -0,0 +1,19 @@
+package chapters.eighteen.input_optput.serialization.systemstaterestore;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.List;
+
+
+public class RecoverCADState {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("CADState.out"));
+        // Читаем объекты в том порядке, в котором они были записаны
+        List<Class<? extends Shape>> shapeTypes = (List<Class<? extends Shape>>) in.readObject();
+        Line.deserializeStaticState(in);
+        List<Shape> shapes = (List<Shape>) in.readObject();
+        System.out.println(shapes);
+    }
+}
