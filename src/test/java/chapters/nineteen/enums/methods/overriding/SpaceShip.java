846d8eb84a90e1da81049247d69ec22ae7de824e
andrey shalagin <andreyshalagin@gmail.com>
2025-03-28 08:35:43 +0300

    Переопределение методов в Enum (toString())

---
+++ src/test/java/chapters/nineteen/enums/methods/overriding/SpaceShip.java
@@ -0,0 +1,16 @@
+package chapters.nineteen.enums.methods.overriding;
+
+public enum SpaceShip {
+    SCOUT, CARGO, TRANSPORT, CRUISER, BATTLESHIP, MOTHERSHIP;
+
+    public String toString() {
+        String id = name();
+        String lower = id.substring(1).toLowerCase();
+        return id.charAt(0) + lower;
+    }
+
+    public static void main(String[] args) {
+        for (SpaceShip ship : values())
+            System.out.println(ship);
+    }
+}
