26f4cf8cbf1ab204d2696281f4edd72f20b320ff
andrey shalagin <andreyshalagin@gmail.com>
2025-04-01 07:31:58 +0300

    Пример реализации категорий перечислений на основе внешнего перечисления (перечисление перечислений)

---
+++ src/test/java/chapters/nineteen/enums/categories/menu/Course.java
@@ -0,0 +1,22 @@
+package chapters.nineteen.enums.categories.menu;
+
+import net.mindview.utils.Enums;
+
+public enum Course {
+    APPETIZER(Food.Appetizer.class),
+    MAINCOURSE(Food.MainCourse.class),
+    DESSERT(Food.Coffee.class),
+    COFFEE(Food.Coffee.class);
+
+    private Food[] values;
+
+    Course(Class<? extends Food> kind) {
+        values = kind.getEnumConstants();
+    }
+
+    public Food randomSelection() {
+        return Enums.random(values);
+    }
+
+
+}
