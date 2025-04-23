000feaed9fd167372d67ee7cc2c41981f7162927
andrey shalagin <andreyshalagin@gmail.com>
2025-04-23 07:20:25 +0300

    Задание 4: пример того, что в @Unit на каждый тестовый метод создаётся отдельный объект

---
+++ src/test/java/chapters/twenty/annotations/atunit/AtUnitExample4.java
@@ -0,0 +1,39 @@
+package chapters.twenty.annotations.atunit;
+
+import chapters.eighteen.input_optput.manageprocesses.OSExecute;
+import chapters.twenty.annotations.base_syntax.Test;
+
+public class AtUnitExample4 {
+    public static int count = 0;
+
+    public int id = count++;
+
+    public AtUnitExample4() {
+        System.out.println("Constructing AtUnitExample4 with id = " + id);
+    }
+
+    public String methodOne() {
+        System.out.println("In methodOne() of AtUnitExample4 with id = " + id);
+        return "This is method 1";
+    }
+
+    public int methodTwo() {
+        System.out.println("In methodTwo() of AtUnitExample4 with id = " + id);
+        System.out.println("This is method 2");
+        return 2;
+    }
+
+    @Test
+    boolean _methodOneTes() {
+        return methodOne().equals("This is method 1");
+    }
+
+    @Test
+    boolean _methodTwoTest() {
+        return methodTwo() == 2;
+    }
+
+    public static void main(String[] args) {
+        OSExecute.command("java chapters.twenty.annotations.atunit.AtUnitExample4");
+    }
+}
