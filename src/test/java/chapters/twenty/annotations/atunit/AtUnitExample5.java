00cda0d4875548cbf7c915997513ab3f290d30e1
andrey shalagin <andreyshalagin@gmail.com>
2025-04-24 08:34:43 +0300

    Методы для инициализации и удаления объекта помечаются аннотациями @TestObjectCreate и @TestObjectCleanup соответственно

---
+++ src/test/java/chapters/twenty/annotations/atunit/AtUnitExample5.java
@@ -0,0 +1,64 @@
+package chapters.twenty.annotations.atunit;
+
+import chapters.eighteen.input_optput.manageprocesses.OSExecute;
+import chapters.twenty.annotations.base_syntax.Test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AtUnitExample5 {
+    private String text;
+
+    public AtUnitExample5(String text) {
+        this.text = text;
+    }
+
+    public String toString() {
+        return text;
+    }
+
+//    @TestProperty
+    static PrintWriter output;
+
+//    @TestProperty
+    static int counter;
+
+//    @TestObjectCreate
+    static AtUnitExample5 create() {
+        String id = Integer.toString(counter++);
+        try {
+            output = new PrintWriter("Test" + id + ".txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new AtUnitExample5(id);
+    }
+
+//    @TestObjectCleanup
+    static void cleanUp(AtUnitExample5 topj) {
+        System.out.println("Running cleanup");
+        output.close();
+    }
+
+    @Test
+    boolean test1() {
+        output.print("test1");
+        return true;
+    }
+
+    @Test
+    boolean test2() {
+        output.print("test2");
+        return true;
+    }
+
+    @Test
+    boolean test3() {
+        output.print("test3");
+        return true;
+    }
+
+    public static void main(String[] args) {
+        OSExecute.command("java chapters.twenty.annotations.atunit.AtUnitExample5");
+    }
+}
