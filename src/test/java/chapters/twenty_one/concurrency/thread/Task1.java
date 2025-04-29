5a4f20906ba8debc6167aeacf750345e19405846
andrey shalagin <andreyshalagin@gmail.com>
2025-04-29 07:20:15 +0300

    Задание 1: запуск нескольких задач в отдельных потоках

---
+++ src/test/java/chapters/twenty_one/concurrency/thread/Task1.java
@@ -0,0 +1,30 @@
+package chapters.twenty_one.concurrency.thread;
+
+public class Task1 implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+
+    public Task1() {
+        System.out.println("Message into constructor. " + this);
+    }
+
+    public String toString() {
+        return "Object [Task1]: #" + id;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("#" + i + " message into run(). " + this);
+            Thread.yield();
+        }
+
+        System.out.println(this + " done job!");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Task1()).start();
+        }
+    }
+}
