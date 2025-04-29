a54e6808e5d5d6482c21fd16a49a32fc3856e50a
andrey shalagin <andreyshalagin@gmail.com>
2025-04-29 07:42:03 +0300

    Создание потоков через объект ExecutorService: CachedThreadPool

---
+++ src/test/java/chapters/twenty_one/concurrency/thredpool/CachedThreadPool.java
@@ -0,0 +1,16 @@
+package chapters.twenty_one.concurrency.thredpool;
+
+import chapters.twenty_one.concurrency.runnable.LiftOff;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new LiftOff());
+        }
+        exec.shutdown();
+    }
+}
