7571cdd7412f5fe4bfab386450b55bfd35e6a126
andrey shalagin <andreyshalagin@gmail.com>
2025-04-29 08:04:07 +0300

    Задание 3: реализация задания 1 на 3-х разных видах ThreadExecutor:  - CachedThreadPool  - FixedThreadPool  - SingleThreadExecutor

---
+++ src/test/java/chapters/twenty_one/concurrency/tasks/task_3/Task3SingleThreadExecutor.java
@@ -0,0 +1,16 @@
+package chapters.twenty_one.concurrency.tasks.task_3;
+
+import chapters.twenty_one.concurrency.runnable.LiftOff;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Task3SingleThreadExecutor {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 4; i++) {
+            executorService.execute(new LiftOff());
+        }
+        executorService.shutdown();
+    }
+}
