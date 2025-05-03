623bafcbd5136a81c6a79ec60e06fcea3eb39606
andrey shalagin <andreyshalagin@gmail.com>
2025-05-03 13:08:41 +0300

    Сокрытие потокового кода внутри внутреннего класса, реализующего интерфейс Runnable

--- src/test/java/chapters/twenty_one/concurrency/variants_of_implementation/inner_classes/ThreadVariations.java
+++ src/test/java/chapters/twenty_one/concurrency/variants_of_implementation/inner_classes/ThreadVariations.java
@@ -4,5 +4,7 @@ public class ThreadVariations {
     public static void main(String[] args) {
         new InnerThread1("InnerThread1");
         new InnerThread2("InnerThread2");
+
+        new InnerRunnable1("InnerRunnable1");
     }
 }
