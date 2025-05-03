08fa062fd84aeff3ff498c0ed9d6016afdc58eb3
andrey shalagin <andreyshalagin@gmail.com>
2025-05-03 07:37:42 +0300

    Разновидности реализации: - реализация на основе наследования от Thread (в данной идиоме наследование от ещё одного класса исключено)

---
+++ src/test/java/chapters/twenty_one/concurrency/SimpleThread.java
@@ -0,0 +1,30 @@
+package chapters.twenty_one.concurrency;
+
+public class SimpleThread extends Thread {
+    private int countdown = 5;
+    private static int threadCount = 0;
+
+    public SimpleThread() {
+        // сохранение имени потока
+        super(String.valueOf(++threadCount));
+        start();
+    }
+
+    public String toString() {
+        return "#" + getName() + "(" + countdown + ")";
+    }
+
+    public void run() {
+        while (true) {
+            System.out.println(this);
+            if (--countdown == 0)
+                return;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            new SimpleThread();
+        }
+    }
+}
