902a7ecc2fdfd0e785d6ccfbbb2021475928f43b
andrey shalagin <andreyshalagin@gmail.com>
2025-05-08 07:25:19 +0300

    Исключения, выброшенные задачей, выполняющейся в потоке - не перехватываются

---
+++ src/test/java/chapters/twenty_one/concurrency/catching_exceptions/unhandled/ExceptionThread.java
@@ -0,0 +1,20 @@
+package chapters.twenty_one.concurrency.catching_exceptions.unhandled;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExceptionThread implements Runnable {
+    @Override
+    public void run() {
+        throw new RuntimeException();
+    }
+
+    public static void main(String[] args) {
+        try {
+            ExecutorService exec = Executors.newCachedThreadPool();
+            exec.execute(new ExceptionThread());
+        } catch (RuntimeException e) {
+            System.out.println("Exception has been handled"); // Эта команда НЕ БУДЕТ выполняться
+        }
+    }
+}
