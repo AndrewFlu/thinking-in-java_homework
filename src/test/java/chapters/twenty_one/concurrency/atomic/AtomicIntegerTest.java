b321e53baa92ba4225ab9e8d17f7dc6e48429759
andrey shalagin <andreyshalagin@gmail.com>
2025-05-24 06:40:26 +0300

    Использование атомарных классов на примере AtomicInteger

---
+++ src/test/java/chapters/twenty_one/concurrency/atomic/AtomicIntegerTest.java
@@ -0,0 +1,47 @@
+package chapters.twenty_one.concurrency.atomic;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerTest implements Runnable {
+
+    private final AtomicInteger i = new AtomicInteger(0);
+
+    private int getValue() {
+        return i.get();
+    }
+
+    private void evenAndIncrement() {
+        i.addAndGet(2);
+    }
+
+    @Override
+    public void run() {
+        while (true)
+            evenAndIncrement();
+    }
+
+    public static void main(String[] args) {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.err.println("Aborting");
+                System.exit(0);
+            }
+        }, 4000); // Завершение через 4 секунды
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+        AtomicIntegerTest ait = new AtomicIntegerTest();
+        exec.execute(ait);
+        while (true) {
+            int val = ait.getValue();
+            if (val % 2 != 0) {
+                System.out.println(val);
+                System.exit(0);
+            }
+        }
+    }
+}
