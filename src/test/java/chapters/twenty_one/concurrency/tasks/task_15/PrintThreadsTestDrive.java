00e4095755f59f180cafa8383edc62309196c7cf
andrey shalagin <andreyshalagin@gmail.com>
2025-05-31 09:01:47 +0300

    Задание 15_2: синхронизация на разных объектах позволяет потокам работать независимо друг от друга

--- src/test/java/chapters/twenty_one/concurrency/tasks/task_15/PrintThreadsTestDrive.java
+++ src/test/java/chapters/twenty_one/concurrency/tasks/task_15/PrintThreadsTestDrive.java
@@ -6,26 +6,11 @@ import java.util.concurrent.Executors;
 public class PrintThreadsTestDrive {
     public static void main(String[] args) {
         PrintUtilities pu = new PrintUtilities();
-        Printer printer = new Printer(25);
-//        pu.printDocs(new Printer(4));
-
-//        new Thread(){
-//            @Override
-//            public void run() {
-//                pu.printDocs(printer);
-//            }
-//        }.start();
-//
-//        new Thread() {
-//            @Override
-//            public void run() {
-//                pu.printNotes(printer);
-//            }
-//        }.start();
+        Printer printer = new Printer(2);
 
         ExecutorService exec = Executors.newCachedThreadPool();
-        exec.execute(() -> pu.printNotes(printer));
-        exec.execute(() -> pu.printImages(printer));
+        exec.execute(() -> pu.printNotes(new Printer(2)));
+        exec.execute(() -> pu.printImages(new Printer(50)));
         exec.shutdown();
         pu.printDocs(printer);
 
