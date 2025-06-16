9b7fe2e144c8dd1ee2215dda3f05b69e9c603523
andrey shalagin <andreyshalagin@gmail.com>
2025-06-16 21:19:18 +0300

    Синхронизированные очереди

---
+++ src/test/java/chapters/twenty_one/concurrency/producer_consumer/blockingqueues/LiftOffRunner.java
@@ -0,0 +1,35 @@
+package chapters.twenty_one.concurrency.producer_consumer.blockingqueues;
+
+import chapters.twenty_one.concurrency.runnable.LiftOff;
+
+import java.util.concurrent.BlockingQueue;
+
+public class LiftOffRunner implements Runnable {
+    private BlockingQueue<LiftOff> rockets;
+
+    public LiftOffRunner(BlockingQueue<LiftOff> queue) {
+        this.rockets = queue;
+    }
+
+    public void add(LiftOff io) {
+        try {
+            rockets.put(io);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupting during put()");
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                LiftOff rocket = rockets.take();
+                rocket.run(); // использовать этот поток
+            }
+
+        } catch (InterruptedException e) {
+            System.out.println("Waking from take()");
+        }
+        System.out.println("Exiting LiftOffRunner");
+    }
+}
