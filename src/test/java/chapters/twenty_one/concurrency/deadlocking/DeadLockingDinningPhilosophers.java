1441cbe9434410ee934c3b42896299fe8ac2198f
andrey shalagin <andreyshalagin@gmail.com>
2025-06-21 08:34:23 +0300

    Пример взаимной блокировки. Для воспроизведения проблемы необходимо запустить программу со следующими параметрами: 0 5 timeout Проблема заклюбчается в выполнении всех 4 обстоятельств: 1) взаимное исключение (один ресур не является разделяемым (одной палочкой не могут есть два философа)) 2) один процесс должен удерживать ресур и ожидать освобождения ресурса, удерживаемого другим потоком 3) ресурс нельзя отбирать у процесса 4) должно произойти круговое ожидание (замыкание ожидания на ожидании первого процесса)

---
+++ src/test/java/chapters/twenty_one/concurrency/deadlocking/DeadLockingDinningPhilosophers.java
@@ -0,0 +1,35 @@
+package chapters.twenty_one.concurrency.deadlocking;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class DeadLockingDinningPhilosophers {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        int pounder = 5;
+        if (args.length > 0)
+            pounder = Integer.parseInt(args[0]);
+
+        int size = 5;
+        if (args.length > 1)
+            size = Integer.parseInt(args[1]);
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+        Chopstick[] sticks = new Chopstick[size];
+        for (int i = 0; i < size; i++)
+            sticks[i] = new Chopstick();
+
+        for (int i = 0; i < size; i++)
+            exec.execute(new Philosopher(sticks[i], sticks[(i + 1) % size], i, pounder));
+
+        if (args.length == 3 && args[2].equals("timeout"))
+            TimeUnit.SECONDS.sleep(5);
+        else {
+            System.out.println("Press any key to quit");
+            System.in.read();
+        }
+        exec.shutdownNow();
+    }
+}
