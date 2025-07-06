8b27c4e715077eda6398c4317437e991b7cd7408
andrey shalagin <andreyshalagin@gmail.com>
2025-07-06 07:29:51 +0300

    Пул объектов, который управляет ограниченным набором объектови позволяет захватить объект для использования, а потом вернуть обратно после завершения работы с ним

---
+++ src/test/java/chapters/twenty_one/concurrency/semaphore/Pool.java
@@ -0,0 +1,56 @@
+package chapters.twenty_one.concurrency.semaphore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+public class Pool<T> {
+    private int size;
+    private List<T> items = new ArrayList<>();
+    private volatile boolean[] checkedOut;
+    private Semaphore available;
+
+    public Pool(Class<T> classObject, int size) {
+        this.size = size;
+        checkedOut = new boolean[size];
+        available = new Semaphore(size, true);
+        // Заполнение пула объектами
+        for (int i = 0; i < size; i++)
+            try {
+                // Предполагается конструктор по умолчанию
+                items.add(classObject.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+    }
+
+    public T checkOut() throws InterruptedException {
+        available.acquire();
+        return getItem();
+    }
+
+    public void checkIn(T x) {
+        if (releaseItem(x))
+            available.release();
+    }
+
+    private synchronized T getItem() {
+        for (int i = 0; i < size; i++) {
+            if (!checkedOut[i])
+                checkedOut[i] = true;
+            return items.get(i);
+        }
+        return null; // Семафор не позволяет перейти сюда
+    }
+
+    private synchronized boolean releaseItem(T item) {
+        int index = items.indexOf(item);
+        if (index == -1)
+            return false; // Нет в списке
+        if (checkedOut[index]) {
+            checkedOut[index] = false;
+            return true;
+        }
+        return false; // Объект не был выдан
+    }
+}
