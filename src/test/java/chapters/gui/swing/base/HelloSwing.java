c9125a132dafbf5c7449a03dd5df012d9a684501
andrey shalagin <andreyshalagin@gmail.com>
2025-07-30 08:23:14 +0300

    Знакомство с Swing: простейшее окно

---
+++ src/test/java/chapters/gui/swing/base/HelloSwing.java
@@ -0,0 +1,12 @@
+package chapters.gui.swing.base;
+
+import javax.swing.*;
+
+public class HelloSwing {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello Swing");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+}
