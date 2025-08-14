2f4ed709a16210a69233e8e5359b3a65da504127
andrey shalagin <andreyshalagin@gmail.com>
2025-08-14 11:36:28 +0300

    Программа с применением Панели вкладок (JTabbedPane). В панель вкладок можно добавлять табы (метод addTab(String tabName, JButton tabButton))

---
+++ src/test/java/chapters/gui/swing/components/TabPaneFrame.java
@@ -0,0 +1,43 @@
+package chapters.gui.swing.components;
+
+import chapters.gui.swing.utils.SwingConsole;
+import com.sun.codemodel.internal.fmt.JTextFile;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+
+/**
+ * Демонстрация панели вкладок
+ */
+public class TabPaneFrame extends JFrame {
+
+    private String actions[] = {
+            "Отложить", "Заказать", "Купить",
+            "Сравнить", "Характеристики", "Отзывы",
+            "Поделиться", "Скопировать ссылку"
+    };
+    private JTabbedPane tabs = new JTabbedPane();
+    private JTextField txt = new JTextField(20);
+
+    public TabPaneFrame() throws HeadlessException {
+        int i = 0;
+        for (String action : actions) {
+            tabs.addTab(actions[i], new JButton("Область вкладки с индексом " + i++));
+        }
+        tabs.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                txt.setText("Выбрана вкладка с индексом: " + tabs.getSelectedIndex());
+            }
+        });
+
+        add(BorderLayout.SOUTH, txt);
+        add(tabs);
+    }
+
+    public static void main(String[] args) {
+        SwingConsole.run(new TabPaneFrame(), 500, 350);
+    }
+}
