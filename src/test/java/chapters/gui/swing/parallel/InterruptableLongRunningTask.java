d0ba8df98b9952ceccd71e25004fdde665c9d9c9
andrey shalagin <andreyshalagin@gmail.com>
2025-08-20 11:03:11 +0300

    Пример оптимизированной программы с длительноым выполнением операций: выполнение продолжительных дейстивй (задач) реализовано через Executor (singleThreadExecutor)

---
+++ src/test/java/chapters/gui/swing/parallel/InterruptableLongRunningTask.java
@@ -0,0 +1,72 @@
+package chapters.gui.swing.parallel;
+
+import chapters.gui.swing.utils.SwingConsole;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Выполнение продлжительных задач в потоках
+ */
+public class InterruptableLongRunningTask extends JFrame {
+
+    private JButton b1 = new JButton("Start Long Running Task");
+    private JButton b2 = new JButton("End Long Running Task");
+
+    // используется однопоточный объект экзекьютора,
+    // который ставит ожидающие задачи в очередь
+    // и выполняет их одну за другой
+    ExecutorService exec = Executors.newSingleThreadExecutor();
+
+    public InterruptableLongRunningTask() throws HeadlessException {
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Task task = new Task();
+                exec.execute(task);
+                System.out.println(task + " added to the queue");
+
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exec.shutdownNow(); // Силовое решение
+                exec = Executors.newSingleThreadExecutor();
+            }
+        });setLayout(new FlowLayout());
+        add(b1);
+        add(b2);
+    }
+
+    public static void main(String[] args) {
+        SwingConsole.run(new InterruptableLongRunningTask(), 550, 250);
+    }
+
+    private class Task implements Runnable {
+        private static int counter = 0;
+        private final int id = counter++;
+
+        @Override
+        public void run() {
+            System.out.println(this + " started");
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                System.out.println(this + " interrupted");
+                return;
+            }
+            System.out.println(this + " completed");
+        }
+
+        @Override
+        public String toString() {
+            return "Task " + id;
+        }
+    }
+}
