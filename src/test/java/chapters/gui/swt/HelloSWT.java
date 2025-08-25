8fcf2ef82818f3505ff8e1e60f707b6a7ce80ca0
andrey shalagin <andreyshalagin@gmail.com>
2025-08-25 08:21:32 +0300

    Простейшая программа на SWT

---
+++ src/test/java/chapters/gui/swt/HelloSWT.java
@@ -0,0 +1,15 @@
+package chapters.gui.swt;
+import org.eclipse.swt.widgets.*;
+
+public class HelloSWT {
+    public static void main(String[] args) {
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setText("Hi there, SWT!"); // Заголовок
+        shell.open();
+        while(!shell.isDisposed())
+            if (!display.readAndDispatch())
+                display.sleep();
+        display.dispose();
+    }
+}
