ab1f2ab62a8dd30a7d6c7bfb106f3ae279e2e945
Andrey Shalagin <andreyshalagin@gmail.com>
2025-08-25 20:07:29 +0300

    Демонстрация того, что Shell представляет собой окно программы, которое работает в ГЛАВНОМ (одном) потоке. И даже если создать несколько окон, то при закрытии одного окна — закроются все окна

---
+++ src/test/java/chapters/gui/swt/ShellsAreMainWindows.java
@@ -0,0 +1,29 @@
+package chapters.gui.swt;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class ShellsAreMainWindows {
+    static Shell[] shells = new Shell[10];
+
+    public static void main(String[] args) {
+        Display display = new Display();
+        for (int i = 0; i < shells.length; i++) {
+            shells[i] = new Shell(display);
+            shells[i].setText("Shell #" + i);
+            shells[i].open();
+        }
+        while(!shellIsDisposed())
+            if (!display.readAndDispatch())
+                display.sleep();
+        display.dispose();
+    }
+
+    private static boolean shellIsDisposed() {
+        for (int i = 0; i < shells.length; i++) {
+            if (shells[i].isDisposed())
+                return true;
+        }
+        return false;
+    }
+}
